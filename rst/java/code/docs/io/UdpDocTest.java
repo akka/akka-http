akukday/akka

rename UdpFF->Udp and the other UdpConnected, see #3058

akka-docs/rst/java/code/docs/io/UdpDocTest.java
@@ -9,8 +9,8 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.UntypedActor;
 import akka.io.Inet;
-import akka.io.UdpFF;
-import akka.io.UdpFFMessage;
+import akka.io.Udp;
+import akka.io.UdpMessage;
 import akka.io.UdpSO;
 import akka.util.ByteString;
 
@@ -24,26 +24,26 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 
-public class IOUdpFFDocTest {
+public class UdpDocTest {
     static public class Demo extends UntypedActor {
         public void onReceive(Object message) {
             //#manager
-            final ActorRef udpFF = UdpFF.get(system).manager();
+            final ActorRef udpFF = Udp.get(system).manager();
             //#manager
 
             //#simplesend
-            udpFF.tell(UdpFFMessage.simpleSender(), getSelf());
+            udpFF.tell(UdpMessage.simpleSender(), getSelf());
 
             // ... or with socket options:
             final List<Inet.SocketOption> options = new ArrayList<Inet.SocketOption>();
             options.add(UdpSO.broadcast(true));
-            udpFF.tell(UdpFFMessage.simpleSender(), getSelf());
+            udpFF.tell(UdpMessage.simpleSender(), getSelf());
             //#simplesend
 
             ActorRef simpleSender = null;
 
             //#simplesend-finish
-            if (message instanceof UdpFF.SimpleSendReady) {
+            if (message instanceof Udp.SimpleSendReady) {
                 simpleSender = getSender();
             }
             //#simplesend-finish
@@ -51,33 +51,33 @@ public class IOUdpFFDocTest {
             final ByteString data = ByteString.empty();
 
             //#simplesend-send
-            simpleSender.tell(UdpFFMessage.send(data, new InetSocketAddress("127.0.0.1", 7654)), getSelf());
+            simpleSender.tell(UdpMessage.send(data, new InetSocketAddress("127.0.0.1", 7654)), getSelf());
             //#simplesend-send
 
             final ActorRef handler = getSelf();
 
             //#bind
-            udpFF.tell(UdpFFMessage.bind(handler, new InetSocketAddress("127.0.0.1", 9876)), getSelf());
+            udpFF.tell(UdpMessage.bind(handler, new InetSocketAddress("127.0.0.1", 9876)), getSelf());
             //#bind
 
             ActorRef udpWorker = null;
 
             //#bind-finish
-            if (message instanceof UdpFF.Bound) {
+            if (message instanceof Udp.Bound) {
                 udpWorker = getSender();
             }
             //#bind-finish
 
             //#bind-receive
-            if (message instanceof UdpFF.Received) {
-                final UdpFF.Received rcvd = (UdpFF.Received) message;
+            if (message instanceof Udp.Received) {
+                final Udp.Received rcvd = (Udp.Received) message;
                 final ByteString payload = rcvd.data();
                 final InetSocketAddress sender = rcvd.sender();
             }
             //#bind-receive
 
             //#bind-send
-            udpWorker.tell(UdpFFMessage.send(data, new InetSocketAddress("127.0.0.1", 7654)), getSelf());
+            udpWorker.tell(UdpMessage.send(data, new InetSocketAddress("127.0.0.1", 7654)), getSelf());
             //#bind-send
         }
     }

akka/akka-http

rst/java/code/docs/io/UdpDocTest.java
@@ -9,8 +9,8 @@ import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.UntypedActor;
 import akka.io.Inet;
-import akka.io.UdpFF;
-import akka.io.UdpFFMessage;
+import akka.io.Udp;
+import akka.io.UdpMessage;
 import akka.io.UdpSO;
 import akka.util.ByteString;
 
@@ -24,26 +24,26 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 
-public class IOUdpFFDocTest {
+public class UdpDocTest {
     static public class Demo extends UntypedActor {
         public void onReceive(Object message) {
             //#manager
-            final ActorRef udpFF = UdpFF.get(system).manager();
+            final ActorRef udp = Udp.get(system).manager();
             //#manager
 
             //#simplesend
-            udpFF.tell(UdpFFMessage.simpleSender(), getSelf());
+            udp.tell(UdpMessage.simpleSender(), getSelf());
 
             // ... or with socket options:
             final List<Inet.SocketOption> options = new ArrayList<Inet.SocketOption>();
             options.add(UdpSO.broadcast(true));
-            udpFF.tell(UdpFFMessage.simpleSender(), getSelf());
+            udp.tell(UdpMessage.simpleSender(), getSelf());
             //#simplesend
 
             ActorRef simpleSender = null;
 
             //#simplesend-finish
-            if (message instanceof UdpFF.SimpleSendReady) {
+            if (message instanceof Udp.SimpleSendReady) {
                 simpleSender = getSender();
             }
             //#simplesend-finish
@@ -51,33 +51,33 @@ public class IOUdpFFDocTest {
             final ByteString data = ByteString.empty();
 
             //#simplesend-send
-            simpleSender.tell(UdpFFMessage.send(data, new InetSocketAddress("127.0.0.1", 7654)), getSelf());
+            simpleSender.tell(UdpMessage.send(data, new InetSocketAddress("127.0.0.1", 7654)), getSelf());
             //#simplesend-send
 
             final ActorRef handler = getSelf();
 
             //#bind
-            udpFF.tell(UdpFFMessage.bind(handler, new InetSocketAddress("127.0.0.1", 9876)), getSelf());
+            udp.tell(UdpMessage.bind(handler, new InetSocketAddress("127.0.0.1", 9876)), getSelf());
             //#bind
 
             ActorRef udpWorker = null;
 
             //#bind-finish
-            if (message instanceof UdpFF.Bound) {
+            if (message instanceof Udp.Bound) {
                 udpWorker = getSender();
             }
             //#bind-finish
 
             //#bind-receive
-            if (message instanceof UdpFF.Received) {
-                final UdpFF.Received rcvd = (UdpFF.Received) message;
+            if (message instanceof Udp.Received) {
+                final Udp.Received rcvd = (Udp.Received) message;
                 final ByteString payload = rcvd.data();
                 final InetSocketAddress sender = rcvd.sender();
             }
             //#bind-receive
 
             //#bind-send
-            udpWorker.tell(UdpFFMessage.send(data, new InetSocketAddress("127.0.0.1", 7654)), getSelf());
+            udpWorker.tell(UdpMessage.send(data, new InetSocketAddress("127.0.0.1", 7654)), getSelf());
             //#bind-send
         }
     }