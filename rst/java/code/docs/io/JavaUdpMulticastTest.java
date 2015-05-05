125f2b4492a9905477c2a0399022c933ffcd75e1
Patrik Nordwall <patrik.nordwall@gmail.com>
2015-05-05 16:44:49 +0200

    !pro #17380 Build with Java 8
    * genjavadoc adjustments for java8

---
+++ rst/java/code/docs/io/JavaUdpMulticastTest.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (C) 2009-2015 Typesafe Inc. <http://www.typesafe.com>
+ */
+
+package docs.io;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.io.Udp;
+import akka.testkit.JavaTestKit;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+import java.util.Random;
+
+public class JavaUdpMulticastTest {
+
+    static ActorSystem system;
+
+    @BeforeClass
+    public static void setup() {
+        system = ActorSystem.create("JavaUdpMulticastTest");
+    }
+
+    @Test
+    public void testUdpMulticast() throws Exception {
+        new JavaTestKit(system) {{
+            NetworkInterface ipv6Iface = null;
+            for (Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces(); interfaces.hasMoreElements() && ipv6Iface == null;) {
+                NetworkInterface interf = interfaces.nextElement();
+                for (Enumeration<InetAddress> addresses = interf.getInetAddresses(); addresses.hasMoreElements() && ipv6Iface == null;) {
+                    InetAddress address = addresses.nextElement();
+                    if (address instanceof Inet6Address) {
+                        ipv6Iface = interf;
+                    }
+                }
+            }
+
+            // host assigned link local multicast address http://tools.ietf.org/html/rfc3307#section-4.3.2
+            // generate a random 32 bit multicast address with the high order bit set
+            final String randomAddress = Long.toHexString(((long) Math.abs(new Random().nextInt())) | (1L << 31)).toUpperCase();
+            final StringBuilder groupBuilder = new StringBuilder("FF02:");
+            for (int i = 0; i < 2; i += 1) {
+                groupBuilder.append(":");
+                groupBuilder.append(randomAddress.subSequence(i * 4, i * 4 + 4));
+            }
+            final String group = groupBuilder.toString();
+            final Integer port = TestUtils.temporaryUdpIpv6Port(ipv6Iface);
+            final String msg = "ohi";
+            final ActorRef sink = getRef();
+            final String iface = ipv6Iface.getName();
+
+            final ActorRef listener = system.actorOf(Props.create(JavaUdpMulticast.Listener.class, iface, group, port, sink));
+            expectMsgClass(Udp.Bound.class);
+            final ActorRef sender = system.actorOf(Props.create(JavaUdpMulticast.Sender.class, iface, group, port, msg));
+            expectMsgEquals(msg);
+
+            // unbind
+            system.stop(listener);
+        }};
+    }
+
+    @AfterClass
+    public static void tearDown() {
+        JavaTestKit.shutdownActorSystem(system);
+        system = null;
+    }
+}
---
+++ rst/scala/code/docs/io/ScalaUdpMulticastSpec.scala
@@ -0,0 +1,59 @@
+/**
+ * Copyright (C) 2009-2015 Typesafe Inc. <http://www.typesafe.com>
+ */
+
+package docs.io
+
+import java.net.{ Inet6Address, InetSocketAddress, NetworkInterface, StandardProtocolFamily }
+import java.nio.channels.DatagramChannel
+import scala.util.Random
+
+import akka.actor.{ ActorSystem, Props }
+import akka.io.Udp
+import akka.testkit.TestKit
+import org.scalatest.{ BeforeAndAfter, WordSpecLike }
+
+import scala.collection.JavaConversions.enumerationAsScalaIterator
+
+class ScalaUdpMulticastSpec extends TestKit(ActorSystem("ScalaUdpMulticastSpec")) with WordSpecLike with BeforeAndAfter {
+
+  "listener" should {
+    "send message back to sink" in {
+      val Some(ipv6Iface) = NetworkInterface.getNetworkInterfaces.collectFirst {
+        case iface if iface.getInetAddresses.exists(_.isInstanceOf[Inet6Address]) => iface
+      }
+
+      // host assigned link local multicast address http://tools.ietf.org/html/rfc3307#section-4.3.2
+      // generate a random 32 bit multicast address with the high order bit set
+      val randomAddress: String = (Random.nextInt().abs.toLong | (1L << 31)).toHexString.toUpperCase
+      val group = randomAddress.grouped(4).mkString("FF02::", ":", "")
+      val port = TestUtils.temporaryUdpIpv6Port(ipv6Iface)
+      val msg = "ohi"
+      val sink = testActor
+      val iface = ipv6Iface.getName
+
+      val listener = system.actorOf(Props(classOf[Listener], iface, group, port, sink))
+      expectMsgType[Udp.Bound]
+      val sender = system.actorOf(Props(classOf[Sender], iface, group, port, msg))
+      expectMsg(msg)
+
+      // unbind
+      system.stop(listener)
+    }
+  }
+
+  def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+  }
+
+}
+
+object TestUtils {
+  def temporaryUdpIpv6Port(iface: NetworkInterface) = {
+    val serverSocket = DatagramChannel.open(StandardProtocolFamily.INET6).socket()
+    serverSocket.bind(new InetSocketAddress(iface.getInetAddresses.nextElement(), 0))
+    val port = serverSocket.getLocalPort
+    serverSocket.close()
+    port
+  }
+}
