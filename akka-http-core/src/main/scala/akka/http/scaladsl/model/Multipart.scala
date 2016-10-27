akka/akka

commit bf1631d74ec796f412b9c777bd60e356538d66a3

+htc #333 deprecate Multipart.toEntity and introduce overloads without charset

akka-http-core/src/main/scala/akka/http/scaladsl/model/Multipart.scala
@@ -6,13 +6,15 @@ package akka.http.scaladsl.model
 import java.io.File
 import java.nio.file.Path
 import java.util.Optional
-import akka.http.impl.util.Util
+
+import akka.http.impl.util.{ DefaultNoLogging, Util }
+
 import scala.concurrent.duration.FiniteDuration
 import scala.concurrent.Future
 import scala.collection.immutable
 import scala.collection.JavaConverters._
 import scala.util.{ Failure, Success, Try }
-import akka.event.{ NoLogging, LoggingAdapter }
+import akka.event.LoggingAdapter
 import akka.stream.impl.ConstantFun
 import akka.stream.Materializer
 import akka.stream.javadsl.{ Source ⇒ JSource }
@@ -23,8 +25,11 @@ import akka.http.impl.engine.rendering.BodyPartRenderer
 import akka.http.javadsl.{ model ⇒ jm }
 import FastFuture._
 import akka.http.impl.util.JavaMapping.Implicits._
+
 import scala.compat.java8.FutureConverters._
 import java.util.concurrent.CompletionStage
+
+import akka.http.javadsl.model.RequestEntity
 
 /**
  * The model of multipart content for media-types `multipart/\*` (general multipart content),
@@ -55,15 +60,39 @@ sealed trait Multipart extends jm.Multipart {
 
   /**
    * Creates a [[akka.http.scaladsl.model.MessageEntity]] from this multipart object.
+   *
+   * @deprecated This variant of `toEntity` is not supported any more. The charset parameter will be ignored. Please use
+   *             one of the other overloads instead.
    */
-  def toEntityWithLog(
-    boundary: String = BodyPartRenderer.randomBoundary())(implicit log: LoggingAdapter = NoLogging): MessageEntity = {
+  @deprecated(
+    message = "This variant of `toEntity` is not supported any more. The charset parameter will be ignored. " +
+    "Please use the variant without specifying the charset.",
+    since = "3.0.0")
+  def toEntity(
+    charset:  HttpCharset = HttpCharsets.`UTF-8`,
+    boundary: String      = BodyPartRenderer.randomBoundary())(implicit log: LoggingAdapter = DefaultNoLogging): MessageEntity =
+    toEntity(boundary, log)
+
+  /**
+   * Creates an entity from this multipart object using the specified boundary and logger.
+   */
+  def toEntity(boundary: String, log: LoggingAdapter): MessageEntity = {
     val chunks =
       parts
         .via(BodyPartRenderer.streamed(boundary, partHeadersSizeHint = 128, log))
         .flatMapConcat(ConstantFun.scalaIdentityFunction)
     HttpEntity.Chunked(mediaType withBoundary boundary, chunks)
   }
+
+  /**
+   * Creates an entity from this multipart object using the specified boundary.
+   */
+  def toEntity(boundary: String): MessageEntity = toEntity(boundary, DefaultNoLogging)
+
+  /**
+   * Creates an entity from this multipart object using a random boundary.
+   */
+  def toEntity: MessageEntity = toEntity(BodyPartRenderer.randomBoundary(), DefaultNoLogging)
 
   /** Java API */
   def getMediaType: jm.MediaType.Multipart = mediaType
@@ -77,8 +106,8 @@ sealed trait Multipart extends jm.Multipart {
     toStrict(FiniteDuration(timeoutMillis, concurrent.duration.MILLISECONDS))(materializer).toJava
 
   /** Java API */
-  def toEntity(boundary: String): jm.RequestEntity =
-    toEntityWithLog(boundary)
+  @deprecated def toEntity(charset: jm.HttpCharset, boundary: String): jm.RequestEntity =
+    toEntity(boundary)
 }
 
 object Multipart {
@@ -96,10 +125,26 @@ object Multipart {
      */
     def strictParts: immutable.Seq[Multipart.BodyPart.Strict]
 
-    override def toEntityWithLog(boundary: String)(implicit log: LoggingAdapter = NoLogging): HttpEntity.Strict = {
+    override def toEntity(charset: HttpCharset, boundary: String)(implicit log: LoggingAdapter = DefaultNoLogging): HttpEntity.Strict =
+      toEntity(boundary, log)
+
+    /**
+     * Creates an entity from this multipart object using the specified boundary and logger.
+     */
+    override def toEntity(boundary: String, log: LoggingAdapter): HttpEntity.Strict = {
       val data = BodyPartRenderer.strict(strictParts, boundary, partHeadersSizeHint = 128, log)
       HttpEntity(mediaType withBoundary boundary, data)
     }
+
+    /**
+     * Creates an entity from this multipart object using the specified boundary.
+     */
+    override def toEntity(boundary: String): HttpEntity.Strict = toEntity(boundary, DefaultNoLogging)
+
+    /**
+     * Creates an entity from this multipart object using a random boundary.
+     */
+    override def toEntity: HttpEntity.Strict = toEntity(BodyPartRenderer.randomBoundary(), DefaultNoLogging)
 
     /** Java API */
     override def getParts: JSource[_ <: jm.Multipart.BodyPart.Strict, AnyRef] =

akka-http-core/src/main/java/akka/http/javadsl/model/Multipart.java
@@ -38,15 +38,46 @@ public interface Multipart {
 
     /**
      * Creates an entity from this multipart object.
+     *
+     * @deprecated This variant of `toEntity` is not supported any more. The charset parameter will be ignored.
+     * Please use the variant without specifying the charset.
+     */
+    @Deprecated
+    RequestEntity toEntity(HttpCharset charset, String boundary);
+
+    /**
+     * Creates an entity from this multipart object using the specified boundary.
      */
     RequestEntity toEntity(String boundary);
+
+    /**
+     * Creates an entity from this multipart object using a random boundary.
+     */
+    RequestEntity toEntity();
 
     interface Strict extends Multipart {
         Source<? extends Multipart.BodyPart.Strict, Object> getParts();
 
         Iterable<? extends Multipart.BodyPart.Strict> getStrictParts();
 
+        /**
+         * Creates an entity from this multipart object.
+         *
+         * @deprecated This variant of `toEntity` is not supported any more. The charset parameter will be ignored.
+         * Please use the variant without specifying the charset.
+         */
+        @Deprecated
         HttpEntity.Strict toEntity(HttpCharset charset, String boundary);
+
+        /**
+         * Creates an entity from this multipart object using the specified boundary.
+         */
+        HttpEntity.Strict toEntity(String boundary);
+
+        /**
+         * Creates an entity from this multipart object using a random boundary.
+         */
+        HttpEntity.Strict toEntity();
     }
 
     interface BodyPart {

akka-http-core/src/main/scala/akka/http/impl/engine/rendering/BodyPartRenderer.scala
@@ -28,7 +28,7 @@ private[http] object BodyPartRenderer {
   def streamed(
     boundary:            String,
     partHeadersSizeHint: Int,
-    log:                 LoggingAdapter): GraphStage[FlowShape[Multipart.BodyPart, Source[ChunkStreamPart, Any]]] =
+    _log:                LoggingAdapter): GraphStage[FlowShape[Multipart.BodyPart, Source[ChunkStreamPart, Any]]] =
     new GraphStage[FlowShape[Multipart.BodyPart, Source[ChunkStreamPart, Any]]] {
       var firstBoundaryRendered = false
 
@@ -37,7 +37,9 @@ private[http] object BodyPartRenderer {
       override val shape: FlowShape[Multipart.BodyPart, Source[ChunkStreamPart, Any]] = FlowShape(in, out)
 
       override def createLogic(inheritedAttributes: Attributes): GraphStageLogic =
-        new GraphStageLogic(shape) with InHandler with OutHandler {
+        new GraphStageLogic(shape) with InHandler with OutHandler with StageLogging {
+          override def logOverride: LoggingAdapter = _log
+
           override def onPush(): Unit = {
             val r = new ByteStringRendering(partHeadersSizeHint)
 

akka-http-core/src/main/scala/akka/http/impl/util/StageLogging.scala
@@ -1,0 +1,65 @@
+/**
+ * Copyright (C) 2016 Lightbend Inc. <http://www.lightbend.com>
+ *
+ * Copied and adapted from akka-remote
+ * https://github.com/akka/akka/blob/c90121485fcfc44a3cee62a0c638e1982d13d812/akka-remote/src/main/scala/akka/remote/artery/StageLogging.scala
+ */
+package akka.http.impl.util
+
+import akka.stream.stage.GraphStageLogic
+import akka.event.LoggingAdapter
+import akka.stream.ActorMaterializer
+import akka.event.NoLogging
+
+// TODO this can be removed when https://github.com/akka/akka/issues/18793 has been implemented
+/**
+ * INTERNAL API
+ */
+private[akka] trait StageLogging { self: GraphStageLogic ⇒
+  def logOverride: LoggingAdapter = DefaultNoLogging
+
+  private var _log: LoggingAdapter = null
+
+  protected def logSource: Class[_] = this.getClass
+
+  def log: LoggingAdapter = {
+    // only used in StageLogic, i.e. thread safe
+    _log match {
+      case null ⇒
+        _log =
+          logOverride match {
+            case DefaultNoLogging ⇒
+              materializer match {
+                case a: ActorMaterializer ⇒ akka.event.Logging(a.system, logSource)
+                case _                    ⇒ NoLogging
+              }
+            case x ⇒ x
+          }
+      case _ ⇒
+    }
+    _log
+  }
+}
+
+/**
+ * A copy of NoLogging that can be used as a place-holder for "logging not explicitly specified".
+ * It can be matched on to be overridden with default behavior.
+ */
+object DefaultNoLogging extends LoggingAdapter {
+  /**
+   * Java API to return the reference to NoLogging
+   * @return The NoLogging instance
+   */
+  def getInstance = this
+
+  final override def isErrorEnabled = false
+  final override def isWarningEnabled = false
+  final override def isInfoEnabled = false
+  final override def isDebugEnabled = false
+
+  final protected override def notifyError(message: String): Unit = ()
+  final protected override def notifyError(cause: Throwable, message: String): Unit = ()
+  final protected override def notifyWarning(message: String): Unit = ()
+  final protected override def notifyInfo(message: String): Unit = ()
+  final protected override def notifyDebug(message: String): Unit = ()
+}

akka-http-core/src/test/scala/akka/http/scaladsl/model/MultipartSpec.scala
@@ -41,7 +41,7 @@ class MultipartSpec extends WordSpec with Matchers with Inside with BeforeAndAft
       val streamed = Multipart.General(
         MediaTypes.`multipart/mixed`,
         Source(Multipart.General.BodyPart(defaultEntity("data"), List(ETag("xzy"))) :: Nil))
-      val result = streamed.toEntityWithLog("boundary")
+      val result = streamed.toEntity("boundary")
       result.contentType shouldBe MediaTypes.`multipart/mixed`.withBoundary("boundary").toContentType
       val encoding = Await.result(result.dataBytes.runWith(Sink.seq), 1.second)
       encoding.map(_.utf8String).mkString shouldBe "--boundary\r\nContent-Type: text/plain; charset=UTF-8\r\nETag: \"xzy\"\r\n\r\ndata\r\n--boundary--"

akka-http/src/main/scala/akka/http/scaladsl/marshalling/MultipartMarshallers.scala
@@ -5,16 +5,18 @@
 package akka.http.scaladsl.marshalling
 
 import java.util.concurrent.ThreadLocalRandom
-import akka.event.{ NoLogging, LoggingAdapter }
+
+import akka.event.{ LoggingAdapter, NoLogging }
 import akka.http.impl.engine.rendering.BodyPartRenderer
+import akka.http.impl.util.DefaultNoLogging
 import akka.http.scaladsl.model._
 
 trait MultipartMarshallers {
-  implicit def multipartMarshaller[T <: Multipart](implicit log: LoggingAdapter = NoLogging): ToEntityMarshaller[T] =
+  implicit def multipartMarshaller[T <: Multipart](implicit log: LoggingAdapter = DefaultNoLogging): ToEntityMarshaller[T] =
     Marshaller strict { value ⇒
       val boundary = randomBoundary()
       val mediaType = value.mediaType withBoundary boundary
-      Marshalling.WithFixedContentType(mediaType.toContentType, () ⇒ value.toEntityWithLog(boundary)(log))
+      Marshalling.WithFixedContentType(mediaType.toContentType, () ⇒ value.toEntity(boundary, log))
     }
 
   /**

akka/akka-http

commit aba8f82d6927faf29dafa4ff79da3188ca814139

Reenable sbt-unidoc settings (#420)
Copy the UnidocRoot and Unidoc auto plugins from Akka adapted to use
akka-http's .sbt based project build. Exclude akka-parsing due to macros
not supported by sbt-unidoc.

Also fixes an issue with javaunidoc introduced in
bf1631d74ec796f412b9c777bd60e356538d66a3:

    [error] /opt/akka-http/akka-http-core/target/java/akka/http/scaladsl/model/Multipart.java:548: error: illegal start of type
    [error]    *
    [error]    ^
    [error] /opt/akka-http/akka-http-core/target/java/akka/http/scaladsl/model/Multipart.java:549: error: = expected
    [error]    * @deprecated . Since .
    [error]    ^
    [error] /opt/akka-http/akka-http-core/target/java/akka/http/scaladsl/model/Multipart.java:549: error: ';' expected
    [error]    * @deprecated . Since .
    [error]     ^
    [error] /opt/akka-http/akka-http-core/target/java/akka/http/scaladsl/model/Multipart.java:549: error: <identifier> expected
    [error]    * @deprecated . Since .
    [error]                           ^
    [error] /opt/akka-http/akka-http-core/target/java/akka/http/scaladsl/model/Multipart.java:550: error: = expected
    [error]   */
    [error]    ^
    [error] /opt/akka-http/akka-http-core/target/java/akka/http/scaladsl/model/Multipart.java:550: error: ';' expected
    [error]   */
    [error]     ^

akka-http-core/src/main/scala/akka/http/scaladsl/model/Multipart.scala
@@ -105,8 +105,17 @@ sealed trait Multipart extends jm.Multipart {
   def toStrict(timeoutMillis: Long, materializer: Materializer): CompletionStage[_ <: jm.Multipart.Strict] =
     toStrict(FiniteDuration(timeoutMillis, concurrent.duration.MILLISECONDS))(materializer).toJava
 
-  /** Java API */
-  @deprecated def toEntity(charset: jm.HttpCharset, boundary: String): jm.RequestEntity =
+  /**
+   * Java API
+   *
+   * @deprecated This variant of `toEntity` is not supported any more. The charset parameter will be ignored. Please use
+   *             one of the other overloads instead.
+   */
+  @deprecated(
+    message = "This variant of `toEntity` is not supported any more. The charset parameter will be ignored. " +
+    "Please use the variant without specifying the charset.",
+    since = "3.0.0")
+  def toEntity(charset: jm.HttpCharset, boundary: String): jm.RequestEntity =
     toEntity(boundary)
 }
 

build.sbt
@@ -36,7 +36,11 @@ lazy val root = Project(
     id = "akka-http-root",
     base = file(".")
   )
-  .enablePlugins(NoPublish)
+  .enablePlugins(UnidocRoot, NoPublish)
+  .settings(
+    // Unidoc doesn't like macros
+    unidocProjectExcludes := Seq(parsing)
+  )
   .aggregate(
     parsing,
     httpCore,

project/Doc.scala
@@ -91,3 +91,56 @@ object ScaladocNoVerificationOfDiagrams extends AutoPlugin {
     Scaladoc.validateDiagrams in Compile := false
   )
 }
+
+/**
+ * Unidoc settings for root project. Adds unidoc command.
+ */
+object UnidocRoot extends AutoPlugin {
+
+  object autoImport {
+    val unidocProjectExcludes = settingKey[Seq[ProjectReference]]("Excluded unidoc projects")
+  }
+  import autoImport._
+
+  object CliOptions {
+    val genjavadocEnabled = CliOption("akka.genjavadoc.enabled", false)
+  }
+
+  override def trigger = noTrigger
+
+  val akkaSettings = UnidocRoot.CliOptions.genjavadocEnabled.ifTrue(Seq(
+    javacOptions in (JavaUnidoc, unidoc) ++= Seq("-Xdoclint:none"),
+    // genjavadoc needs to generate synthetic methods since the java code uses them
+    scalacOptions += "-P:genjavadoc:suppressSynthetic=false",
+    // FIXME: see #18056
+    sources in(JavaUnidoc, unidoc) ~= (_.filterNot(_.getPath.contains("Access$minusControl$minusAllow$minusOrigin")))
+  )).getOrElse(Nil)
+
+  val settings = inTask(unidoc)(Seq(
+    unidocProjectFilter in ScalaUnidoc := inAnyProject -- inProjects(unidocProjectExcludes.value: _*),
+    unidocProjectFilter in JavaUnidoc := inAnyProject -- inProjects(unidocProjectExcludes.value: _*)
+  ))
+
+  override lazy val projectSettings =
+    CliOptions.genjavadocEnabled.ifTrue(scalaJavaUnidocSettings).getOrElse(scalaUnidocSettings) ++
+    settings ++
+    akkaSettings
+}
+
+/**
+ * Unidoc settings for every multi-project. Adds genjavadoc specific settings.
+ */
+object Unidoc extends AutoPlugin {
+
+  override def trigger = allRequirements
+  override def requires = plugins.JvmPlugin
+
+  override lazy val projectSettings = UnidocRoot.CliOptions.genjavadocEnabled.ifTrue(
+    genjavadocExtraSettings ++ Seq(
+      scalacOptions in Compile += "-P:genjavadoc:fabricateParams=true",
+      unidocGenjavadocVersion in Global := "0.10",
+      // FIXME: see #18056
+      sources in(Genjavadoc, doc) ~= (_.filterNot(_.getPath.contains("Access$minusControl$minusAllow$minusOrigin")))
+    )
+  ).getOrElse(Seq.empty)
+}