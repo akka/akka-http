7bf9829c181c49f8a7dfaa15a3cf6349e580b335
Josep Prat <jlprat@gmail.com>
2016-11-20 13:09:52 +0100

    Add extractClientIP example and link to it

--- docs/src/test/java/docs/http/javadsl/server/directives/MiscDirectivesExamplesTest.java
+++ docs/src/test/java/docs/http/javadsl/server/directives/MiscDirectivesExamplesTest.java
@@ -3,15 +3,20 @@
  */
 package docs.http.javadsl.server.directives;
 
+import akka.http.javadsl.model.HttpHeader;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.StatusCodes;
+import akka.http.javadsl.model.headers.RemoteAddress;
 import akka.http.javadsl.server.Route;
 import akka.http.javadsl.unmarshalling.Unmarshaller;
 import akka.http.javadsl.testkit.JUnitRouteTest;
 import org.junit.Test;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class MiscDirectivesExamplesTest extends JUnitRouteTest {
 
@@ -59,6 +64,25 @@ public class MiscDirectivesExamplesTest extends JUnitRouteTest {
     testRoute(route).run(withEntityOfSize.apply(501))
       .assertStatusCode(StatusCodes.OK);
     //#withoutSizeLimitExample
+  }
+
+  @Test
+  public void testExtractClientIP() throws UnknownHostException {
+    //#extractClientIPExample
+    final Route route = extractClientIP(remoteAddr ->
+      complete("Client's IP is " + remoteAddr.getAddress().map(InetAddress::getHostAddress)
+        .orElseGet(() -> "unknown"))
+    );
+
+    final String ip = "192.168.1.2";
+    testRoute(route).run(HttpRequest.GET("/")
+        .addHeader(RemoteAddress
+          .create(akka.http.javadsl.model.RemoteAddress.create(InetAddress.getByName(ip)))))
+      .assertEntity("Client's IP is " + ip);
+
+    testRoute(route).run(HttpRequest.GET("/"))
+      .assertEntity("Client's IP is unknown");
+    //#extractClientIPExample
   }
 
 }
