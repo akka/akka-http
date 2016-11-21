aad7b8d106ae6a79036b08fb201e8a732bfd2aa1
Christian Schmitt <c.schmitt@briefdomain.de>
2016-11-21 18:16:58 +0100

    WIP: adds a more complete state machine and an initial connection close handling

--- akka-http2-support/build.sbt
+++ akka-http2-support/build.sbt
@@ -5,4 +5,4 @@ Dependencies.http2
 fork in run in Test := true
 connectInput in run in Test := true
 
-javaOptions in run in Test += "-javaagent:jetty-alpn-agent-2.0.4.jar"
+javaOptions in run in Test += "-javaagent:jetty-alpn-agent-2.0.5.jar"
--- akka-http2-support/src/main/scala/akka/http/impl/engine/http2/FrameParser.scala
+++ akka-http2-support/src/main/scala/akka/http/impl/engine/http2/FrameParser.scala
@@ -113,12 +113,13 @@ class FrameParser(shouldReadPreface: Boolean) extends ByteStringParser[FrameEven
         ContinuationFrame(streamId, endHeaders, payload.remainingData)
 
       case PING ⇒
+        Http2Compliance.requireZeroStreamId(streamId)
+        Http2Compliance.requireFramSize(payload.remainingSize, 8)
         val ack = Flags.ACK.isSet(flags)
-        // FIXME: ensure data size is 8
         PingFrame(ack, payload.remainingData)
 
       case RST_STREAM ⇒
-        // FIXME: ensure data size
+        Http2Compliance.requireFramSize(payload.remainingSize, 4)
         RstStreamFrame(streamId, ErrorCode.byId(payload.readIntBE()))
 
       case PRIORITY ⇒
