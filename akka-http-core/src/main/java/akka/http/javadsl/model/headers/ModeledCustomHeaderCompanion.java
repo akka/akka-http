ae6b24ad4d3e5d9f08e14ab64882bd82fec63e86
Josep Prat <jlprat@gmail.com>
2017-02-21 22:21:57 +0100

    Custom Header Support #761
    Issue: #761 triggered in PR #844
    First attempt to offer Custom Header Support
    
    Add docs and javadoc

---
+++ akka-http-core/src/main/java/akka/http/javadsl/model/headers/ModeledCustomHeaderCompanion.java
@@ -0,0 +1,43 @@
+package akka.http.javadsl.model.headers;
+
+import akka.http.javadsl.model.HttpHeader;
+import akka.util.Helpers;
+
+import java.util.Optional;
+
+/**
+ * Companion class for the {@link ModeledCustomHeader} class. It offers methods to create {@link ModeledCustomHeader}
+ * from {@link String} or {@link HttpHeader}.
+ */
+public abstract class ModeledCustomHeaderCompanion<H extends ModeledCustomHeader> {
+
+  public abstract String name();
+
+  public String lowercaseName() {
+    return Helpers.toRootLowerCase(name());
+  }
+
+  /**
+   * Parses the value checking that the format is correct.
+   * It may throw if value is not correct
+   */
+  protected abstract H parse(final String value);
+
+  /**
+   * Creates a new {@code ModeledCustomHeader} from the value checking that the format is correct.
+   * It may throw if value is not correct
+   */
+  public H create(final String value) {
+    return parse(value);
+  }
+
+  /**
+   * Transforms an {@code HttpHeader} to this {@code ModeledCustomHeader} if the name and value are correct.
+   */
+  public Optional<H> from(final HttpHeader header) {
+    if (header.lowercaseName().equals(lowercaseName())) {
+      return Optional.of(parse(header.value()));
+    }
+    return Optional.empty();
+  }
+}
