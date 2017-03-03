5b9459a1667e885ecb4642500382ee4ba5513259
Martynas Mickevičius <martynas@2m.lt>
2017-03-03 16:40:19 +0200

    Add join and run http layer bench

--- akka-http-core-bench/src/main/scala/akka.http.scaladsl/HttpBlueprintBenchmark.scala
+++ akka-http-core-bench/src/main/scala/akka.http.scaladsl/HttpBlueprintBenchmark.scala
@@ -6,8 +6,10 @@ package akka.http.scaladsl
 import java.util.concurrent.TimeUnit
 
 import akka.actor.ActorSystem
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{ Flow, Sink, Source }
+import akka.stream.TLSProtocol.{SslTlsInbound, SslTlsOutbound}
+import akka.stream.scaladsl.{Flow, Sink, Source}
 import org.openjdk.jmh.annotations._
 import org.openjdk.jmh.infra.Blackhole
 
@@ -19,13 +21,20 @@ import scala.concurrent.duration._
 @BenchmarkMode(Array(Mode.Throughput))
 class HttpBlueprintBenchmark {
 
-  implicit val system = ActorSystem("HttpBlueprintBuildBenchmark")
+  implicit val system = ActorSystem("HttpBlueprintBenchmark")
   implicit val materializer = ActorMaterializer()
 
   val serverLayer =
     Http().serverLayer()
       .join(Flow.fromSinkAndSource(Sink.ignore, Source.empty))
       .join(Flow.fromSinkAndSource(Sink.ignore, Source.empty))
+
+  val serverHandler =
+    Http().serverLayer().reversed
+      .join(Flow.fromSinkAndSource[HttpRequest, HttpResponse](Sink.ignore, Source.empty))
+
+  val serverConnection =
+    Flow.fromSinkAndSource[SslTlsOutbound, SslTlsInbound](Sink.ignore, Source.empty)
 
   @Benchmark
   def build_server_layer(blackhole: Blackhole): Unit = {
@@ -35,6 +44,11 @@ class HttpBlueprintBenchmark {
   @Benchmark
   def run_server_layer(blackhole: Blackhole): Unit = {
     blackhole.consume(serverLayer.run())
+  }
+
+  @Benchmark
+  def join_and_run_server_layer(blackhole: Blackhole): Unit = {
+    blackhole.consume(serverHandler.join(serverConnection).run())
   }
 
   @TearDown
