adf8d0692745353d44830ccfa8ce34d6a771469b
Johannes Rudolph <johannes.rudolph@gmail.com>
2017-03-30 17:46:24 +0200

    optimize adding `Timeout-Access` header

--- akka-http-core/src/main/scala/akka/http/impl/engine/server/HttpServerBluePrint.scala
+++ akka-http-core/src/main/scala/akka/http/impl/engine/server/HttpServerBluePrint.scala
@@ -256,7 +256,7 @@ private[http] object HttpServerBluePrint {
           val access = new TimeoutAccessImpl(request, initialTimeout, requestEnd,
             getAsyncCallback(emitTimeoutResponse), interpreter.materializer)
           openTimeouts = openTimeouts.enqueue(access)
-          push(requestOut, request.copy(headers = request.headers :+ `Timeout-Access`(access), entity = entity))
+          push(requestOut, request.copy(headers = `Timeout-Access`(access) +: request.headers, entity = entity))
         }
         override def onUpstreamFinish() = complete(requestOut)
         override def onUpstreamFailure(ex: Throwable) = fail(requestOut, ex)
