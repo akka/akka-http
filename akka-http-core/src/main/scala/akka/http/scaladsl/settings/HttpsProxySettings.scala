cd95a580f9d2bfe6fbd692cc0dc2699fc72f1bdc
Sean Callahan <scallahan@twitter.com>
2017-09-14 13:57:07 -0600

    Added ability to set https proxy host and port through application.conf

---
+++ akka-http-core/src/main/scala/akka/http/scaladsl/settings/HttpsProxySettings.scala
@@ -0,0 +1,20 @@
+package akka.http.scaladsl.settings
+
+import akka.annotation.DoNotInherit
+import akka.http.impl.settings.HttpsProxySettingsImpl
+import com.typesafe.config.Config
+
+/**
+ * Public API but not intended for subclassing
+ */
+@DoNotInherit
+abstract class HttpsProxySettings private[akka] extends akka.http.javadsl.settings.HttpsProxySettings() { self: HttpsProxySettingsImpl ⇒
+  def host: String
+  def port: Int
+}
+
+object HttpsProxySettings extends SettingsCompanion[HttpsProxySettings] {
+  override def apply(config: Config): HttpsProxySettings = HttpsProxySettingsImpl(config)
+  override def apply(configOverrides: String): HttpsProxySettings = HttpsProxySettingsImpl(configOverrides)
+}
+
