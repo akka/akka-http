7126f2930657c3ddae86d1c2f5d03499380f56f6
Johannes Rudolph <johannes.rudolph@gmail.com>
2017-09-18 14:30:33 +0200

    =htp add additional streaming tests for new FileUploadDirectives
    Before only Strict BodyParts/HttpEntity were tested which are not
    susceptible to deadlocks. The new version should test the more common
    streaming uploads more comprehensively.

--- akka-http-tests/src/test/scala/akka/http/scaladsl/server/directives/FileUploadDirectivesSpec.scala
+++ akka-http-tests/src/test/scala/akka/http/scaladsl/server/directives/FileUploadDirectivesSpec.scala
@@ -7,11 +7,14 @@ package akka.http.scaladsl.server.directives
 import java.io.File
 import java.nio.file.Files
 
+import akka.NotUsed
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.server.{ MissingFormFieldRejection, RoutingSpec }
 import akka.http.scaladsl.testkit.RouteTestTimeout
+import akka.stream.scaladsl.Source
 import akka.util.ByteString
 import akka.testkit._
+
 import scala.concurrent.Future
 import scala.concurrent.duration._
 
@@ -53,84 +56,103 @@ class FileUploadDirectivesSpec extends RoutingSpec {
   }
 
   "the storeUploadedFile directive" should {
+    val data = s"<int>${"42" * 1000000}</int>" // ~2MB of data
 
-    "write a posted file to a temporary file on disk" in {
+    def withUpload(entityType: String, formDataUpload: Multipart.FormData) =
+      s"write a posted file to a temporary file on disk from $entityType entity" in {
+        @volatile var file: Option[File] = None
 
-      val xml = "<int>42</int>"
-
-      val simpleMultipartUpload =
-        Multipart.FormData(Multipart.FormData.BodyPart.Strict(
-          "fieldName",
-          HttpEntity(ContentTypes.`text/xml(UTF-8)`, xml),
-          Map("filename" → "age.xml")))
-
-      @volatile var file: Option[File] = None
-
-      def tempDest(fileInfo: FileInfo): File = {
-        val dest = File.createTempFile("akka-http-FileUploadDirectivesSpec", ".tmp")
-        file = Some(dest)
-        dest
-      }
-
-      try {
-        Post("/", simpleMultipartUpload) ~> {
-          storeUploadedFile("fieldName", tempDest) {
-            case (info, tmpFile) ⇒
-              complete(info.toString)
-          }
-        } ~> check {
-          file.isDefined === true
-          responseAs[String] === FileInfo("fieldName", "age.xml", ContentTypes.`text/xml(UTF-8)`).toString
-          read(file.get) === xml
+        def tempDest(fileInfo: FileInfo): File = {
+          val dest = File.createTempFile("akka-http-FileUploadDirectivesSpec", ".tmp")
+          file = Some(dest)
+          dest
         }
-      } finally {
-        file.foreach(_.delete())
+
+        try {
+          Post("/", formDataUpload) ~>
+            storeUploadedFile("fieldName", tempDest) { (info, tmpFile) ⇒
+              complete(info.toString)
+            } ~> check {
+              file.isDefined === true
+              responseAs[String] === FileInfo("fieldName", "age.xml", ContentTypes.`text/xml(UTF-8)`).toString
+              read(file.get) === data
+            }
+        } finally {
+          file.foreach(_.delete())
+        }
       }
-    }
+
+    withUpload(
+      "strict",
+      Multipart.FormData(Multipart.FormData.BodyPart.Strict(
+        "fieldName",
+        HttpEntity(ContentTypes.`text/xml(UTF-8)`, data),
+        Map("filename" → "age.xml"))))
+
+    withUpload(
+      "streamed",
+      Multipart.FormData(Multipart.FormData.BodyPart(
+        "fieldName",
+        HttpEntity.IndefiniteLength(ContentTypes.`text/xml(UTF-8)`, inChunks(data)),
+        Map("filename" → "age.xml"))))
   }
 
   "the storeUploadedFiles directive" should {
+    val txt = "42" * 1000000 // ~2MB of data
+    val xml = s"<int>$txt</int>" // ~2MB of data
 
-    "write all posted files to a temporary file on disk" in {
+    def withUpload(entityType: String, formDataUpload: Multipart.FormData) =
+      s"write all posted files to a temporary file on disk from $entityType entity" in {
+        @volatile var files: Seq[File] = Nil
 
-      val txt = "42"
-      val xml = "<int>42</int>"
-
-      val complexMultipartUpload =
-        Multipart.FormData(
-          Multipart.FormData.BodyPart.Strict(
-            "field1",
-            HttpEntity(ContentTypes.`text/plain(UTF-8)`, txt),
-            Map("filename" → "age.txt")),
-          Multipart.FormData.BodyPart.Strict(
-            "field1",
-            HttpEntity(ContentTypes.`text/xml(UTF-8)`, xml),
-            Map("filename" → "age.xml")))
-
-      @volatile var files: Seq[File] = Nil
-
-      def tempDest(fileInfo: FileInfo): File = {
-        val dest = File.createTempFile("akka-http-FileUploadDirectivesSpec", ".tmp")
-        files = files :+ dest
-        dest
-      }
-
-      try {
-        Post("/", complexMultipartUpload) ~> {
-          storeUploadedFiles("fieldName", tempDest) { fields ⇒
-            val content = fields.foldLeft("") {
-              case (acc, (fileInfo, tmpFile)) ⇒
-                acc + read(tmpFile)
-            }
-            complete(content)
-          }
-        } ~> check {
-          responseAs[String] === files.map(read).mkString
+        def tempDest(fileInfo: FileInfo): File = {
+          val dest = File.createTempFile("akka-http-FileUploadDirectivesSpec", ".tmp")
+          files = files :+ dest
+          dest
         }
-      } finally {
-        files.foreach(_.delete())
+
+        try {
+          Post("/", formDataUpload) ~> {
+            storeUploadedFiles("fieldName", tempDest) { fields ⇒
+              val content = fields.foldLeft("") {
+                case (acc, (fileInfo, tmpFile)) ⇒
+                  acc + read(tmpFile)
+              }
+              complete(content)
+            }
+          } ~> check {
+            val response = responseAs[String]
+            response === files.map(read).mkString
+            response === txt + xml
+          }
+        } finally {
+          files.foreach(_.delete())
+        }
       }
-    }
+
+    withUpload(
+      "strict",
+      Multipart.FormData(
+        Multipart.FormData.BodyPart.Strict(
+          "field1",
+          HttpEntity(ContentTypes.`text/plain(UTF-8)`, txt),
+          Map("filename" → "age.txt")),
+        Multipart.FormData.BodyPart.Strict(
+          "field1",
+          HttpEntity(ContentTypes.`text/xml(UTF-8)`, xml),
+          Map("filename" → "age.xml"))))
+
+    withUpload(
+      "streamed",
+      Multipart.FormData(
+        Multipart.FormData.BodyPart(
+          "field1",
+          HttpEntity.IndefiniteLength(ContentTypes.`text/plain(UTF-8)`, inChunks(txt)),
+          Map("filename" → "age.txt")),
+        Multipart.FormData.BodyPart(
+          "field1",
+          HttpEntity.IndefiniteLength(ContentTypes.`text/xml(UTF-8)`, inChunks(xml)),
+          Map("filename" → "age.xml"))))
   }
 
   "the fileUpload directive" should {
@@ -225,18 +247,14 @@ class FileUploadDirectivesSpec extends RoutingSpec {
       extractRequestContext { ctx ⇒
         implicit val mat = ctx.materializer
 
-        fileUploadAll("field1") {
-          case files ⇒
-            val allBytesF = files.foldLeft(Future.successful(ByteString.empty)) {
-              case (accF, (info, bytes)) ⇒
-                val bytesF = bytes.runFold(ByteString.empty) { (all, bytes) ⇒ all ++ bytes }
-                accF.flatMap(acc ⇒ bytesF.map(acc ++ _))
-            }
-
-            // sum all individual file sizes
-            onSuccess(allBytesF) { allBytes ⇒
-              complete(allBytes)
-            }
+        fileUploadAll("field1") { files ⇒
+          complete {
+            Future.traverse(files) { // all the files can be processed in parallel because they are buffered on disk
+              case (info, bytes) ⇒
+                // concatenate all data from a single
+                bytes.runFold(ByteString.empty)(_ ++ _)
+            }.map(_.reduce(_ ++ _)) // and then from all files
+          }
         }
       }
 
@@ -257,28 +275,41 @@ class FileUploadDirectivesSpec extends RoutingSpec {
 
     }
 
-    "stream all file uploads if multiple with the same name are posted" in {
-      val route = echoAsAService
+    val str1 = "42" * 1000000 // ~2MB of data
+    val str2 = "23" * 1000000 // ~2MB of data
 
-      val str1 = "some data"
-      val str2 = "other data"
-      val multipartForm =
-        Multipart.FormData(
-          Multipart.FormData.BodyPart.Strict(
-            "field1",
-            HttpEntity(ContentTypes.`text/plain(UTF-8)`, str1),
-            Map("filename" → "data1.txt")),
-          Multipart.FormData.BodyPart.Strict(
-            "field1",
-            HttpEntity(ContentTypes.`text/plain(UTF-8)`, str2),
-            Map("filename" → "data2.txt")))
+    def withUpload(entityType: String, formDataUpload: Multipart.FormData) =
+      s"stream all file uploads if multiple with the same name are posted as $entityType entities" in {
+        val route = echoAsAService
 
-      Post("/", multipartForm) ~> route ~> check {
-        status shouldEqual StatusCodes.OK
-        responseAs[String] shouldEqual (str1 + str2)
+        Post("/", formDataUpload) ~> route ~> check {
+          status shouldEqual StatusCodes.OK
+          responseAs[String] shouldEqual (str1 + str2)
+        }
       }
+    withUpload(
+      "strict",
+      Multipart.FormData(
+        Multipart.FormData.BodyPart.Strict(
+          "field1",
+          HttpEntity(ContentTypes.`text/plain(UTF-8)`, str1),
+          Map("filename" → "data1.txt")),
+        Multipart.FormData.BodyPart.Strict(
+          "field1",
+          HttpEntity(ContentTypes.`text/plain(UTF-8)`, str2),
+          Map("filename" → "data2.txt"))))
 
-    }
+    withUpload(
+      "streamed",
+      Multipart.FormData(
+        Multipart.FormData.BodyPart(
+          "field1",
+          HttpEntity.IndefiniteLength(ContentTypes.`text/plain(UTF-8)`, inChunks(str1)),
+          Map("filename" → "data1.txt")),
+        Multipart.FormData.BodyPart(
+          "field1",
+          HttpEntity.IndefiniteLength(ContentTypes.`text/plain(UTF-8)`, inChunks(str2)),
+          Map("filename" → "data2.txt"))))
 
     "reject the file upload if the field name is missing" in {
       val route =
@@ -323,4 +354,6 @@ class FileUploadDirectivesSpec extends RoutingSpec {
     }
   }
 
+  private def inChunks(input: String, chunkSize: Int = 10000): Source[ByteString, NotUsed] =
+    Source.fromIterator(() ⇒ input.grouped(10000).map(ByteString(_)))
 }
