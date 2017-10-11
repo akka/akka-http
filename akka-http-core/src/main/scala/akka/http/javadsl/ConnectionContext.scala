f8c4921732b64ecad424d3467bb342f37872e618
Dmitry Aveshin <dmitraver@gmail.com>
2017-10-11 17:54:11 +0200

    Small review fixes.

--- akka-http-core/src/main/scala/akka/http/javadsl/ConnectionContext.scala
+++ akka-http-core/src/main/scala/akka/http/javadsl/ConnectionContext.scala
@@ -61,7 +61,7 @@ abstract class ConnectionContext {
   def isSecure: Boolean
   /** Java API */
 
-  @deprecated("'default-http-port' and 'default-https-port' configuration properties are used instead")
+  @deprecated("'default-http-port' and 'default-https-port' configuration properties are used instead", since = "10.0.11")
   def getDefaultPort: Int
   def sslConfig: Option[AkkaSSLConfig]
 }
