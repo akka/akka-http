6afddfbef29becb7dab7aca01b5a1c03e8b2ce04
tom.walford <tomma2008@hotmail.com>
2017-11-18 12:20:46 +0000

    Cleaned up state management in DeflateDecompressor

--- akka-http/src/main/scala/akka/http/scaladsl/coding/Deflate.scala
+++ akka-http/src/main/scala/akka/http/scaladsl/coding/Deflate.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2016 Lightbend Inc. <http://www.lightbend.com>
+ * Copyright (C) 2009-2017 Lightbend Inc. <http://www.lightbend.com>
  */
 
 package akka.http.scaladsl.coding
@@ -89,20 +89,36 @@ private[http] object DeflateCompressor {
 class DeflateDecompressor(maxBytesPerChunk: Int = Decoder.MaxBytesPerChunkDefault) extends DeflateDecompressorBase(maxBytesPerChunk) {
 
   override def createLogic(attr: Attributes) = new DecompressorParsingLogic {
-    var maybeNoWrappedInflater: Option[Inflater] = None
-    override def inflater: Inflater = maybeNoWrappedInflater.getOrElse(new Inflater())
+    var _inflater: Inflater = _
+    override def inflater: Inflater = if (_inflater != null) {
+      _inflater
+    } else {
+      throw new IllegalStateException("Inflater used before the header was analysed for wrapping")
+    }
 
     override val inflateState = new Inflate(true) {
       override def onTruncation(): Unit = completeStage()
 
       override def parse(reader: ByteStringParser.ByteReader): ParseResult[ByteString] = {
         val data = reader.remainingData
-        maybeNoWrappedInflater = maybeNoWrappedInflater
-          .orElse(data.headOption.filter(b ⇒ (b & 0x0F) != 8)
-            .fold(Some(new Inflater()))(_ ⇒ Some(new Inflater(true))))
+        if (_inflater == null) {
+          _inflater = examineAndBuildInflater(data)
+        }
         super.parse(new ByteStringParser.ByteReader(reader.takeAll()))
       }
     }
+
+    /**
+     * We examine the head byte of the incoming data stream, looking for the standard 0x8 in the first 4 bits of the  header
+     * that marks a byte stream as using a wrapped deflate stream. If we don't find that header, we fall back to the no-wrap
+     * inflater
+     * More details on the structure of wrap and no-wrap headers of Deflate can be found at:
+     * https://www.ietf.org/rfc/rfc1950.txt (wrap) and
+     * https://www.ietf.org/rfc/rfc1951.txt (no-wrap)
+     */
+    private def examineAndBuildInflater(bytes: ByteString): Inflater =
+      bytes.headOption.filter(b ⇒ (b & 0x0F) != 8)
+        .fold(new Inflater())(_ ⇒ new Inflater(true))
 
     override def afterInflate = inflateState
     override def afterBytesRead(buffer: Array[Byte], offset: Int, length: Int): Unit = {}
