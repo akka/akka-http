3f15c62ce0d67cbc9e9d96bfb9c0c4d4df010cd7
Johannes Rudolph <johannes.rudolph@gmail.com>
2017-12-21 11:56:41 +0100

    Put spec reference into docs

--- akka-http-core/src/main/scala/akka/http/scaladsl/model/headers/RetryAfterParameter.scala
+++ akka-http-core/src/main/scala/akka/http/scaladsl/model/headers/RetryAfterParameter.scala
@@ -6,11 +6,13 @@ package akka.http.scaladsl.model.headers
 
 import akka.http.scaladsl.model._
 
-//https://tools.ietf.org/html/rfc7231#section-7.1.3
-/** Defines different values admitted to define a [[`Retry-After`]] header */
+/**
+ * Defines different values admitted to define a [[`Retry-After`]] header.
+ *
+ * Spec: https://tools.ietf.org/html/rfc7231#section-7.1.3
+ */
 sealed abstract class RetryAfterParameter
 final case class RetryAfterDuration(delayInSeconds: Long) extends RetryAfterParameter {
   require(delayInSeconds >= 0, "Retry-after header must not contain a negative delay in seconds")
 }
 final case class RetryAfterDateTime(dateTime: DateTime) extends RetryAfterParameter
-
