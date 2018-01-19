7ec066b6e9625e4d0d3b2a1171e726673186323b
Denys Zadorozhnyi <denys@zadorozhnyi.com>
2018-01-19 16:49:39 +0200

    Add `Tls-session-info` header for http/2 #1432

--- akka-http-core/src/main/scala/akka/http/impl/engine/server/HttpAttributes.scala
+++ akka-http-core/src/main/scala/akka/http/impl/engine/server/HttpAttributes.scala
@@ -5,6 +5,7 @@
 package akka.http.impl.engine.server
 
 import java.net.InetSocketAddress
+import javax.net.ssl.SSLSession
 
 import akka.annotation.InternalApi
 import akka.stream.Attributes
@@ -28,6 +29,11 @@ private[akka] object HttpAttributes {
 
   private[akka] def remoteAddress(address: InetSocketAddress): Attributes =
     Attributes(RemoteAddress(address))
+
+  private[akka] final case class TLSSessionInfo(session: SSLSession) extends Attribute
+
+  private[akka] def tlsSessionInfo(session: SSLSession): Attributes =
+    Attributes(TLSSessionInfo(session))
 
   private[akka] val empty: Attributes =
     Attributes()
