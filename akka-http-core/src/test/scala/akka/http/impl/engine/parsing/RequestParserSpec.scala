03a09d7e0a428169dba332ac0c5dfc3210d19394
Konrad `ktoso` Malawski <konrad.malawski@project13.pl>
2018-01-29 18:56:12 +0900

    Make " " more visible in tests by encoding as \u0020

--- akka-http-core/src/test/scala/akka/http/impl/engine/parsing/RequestParserSpec.scala
+++ akka-http-core/src/test/scala/akka/http/impl/engine/parsing/RequestParserSpec.scala
@@ -258,8 +258,8 @@ abstract class RequestParserSpec(mode: String, newLine: String) extends FreeSpec
       "with incorrect but harmless whitespace after chunk size" in new Test {
         Seq(
           start,
-          """0  
-            |
+          "0\u0020\u0020" +
+         """|
             |""") should generalMultiParseTo(
             Right(baseRequest.withEntity(Chunked(`application/pdf`, source(LastChunk)))))
         closeAfterResponseCompletion shouldEqual Seq(false)
