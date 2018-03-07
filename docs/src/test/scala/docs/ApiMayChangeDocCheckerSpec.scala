921f26c3dcecd4f9fc1a5a39b255427aaa0e2a91
Josep Prat <jlprat@gmail.com>
2018-03-07 21:04:57 +0100

    Fix style and add header
    Cleanup test code

--- docs/src/test/scala/docs/ApiMayChangeDocCheckerSpec.scala
+++ docs/src/test/scala/docs/ApiMayChangeDocCheckerSpec.scala
@@ -1,12 +1,15 @@
+/*
+ * Copyright (C) 2009-2018 Lightbend Inc. <https://www.lightbend.com>
+ */
 package docs
 
 import java.lang.reflect.Method
 
 import akka.annotation.ApiMayChange
 import org.reflections.Reflections
-import org.reflections.scanners.{MethodAnnotationsScanner, TypeAnnotationsScanner}
-import org.reflections.util.{ClasspathHelper, ConfigurationBuilder}
-import org.scalatest.{Matchers, WordSpec}
+import org.reflections.scanners.{ MethodAnnotationsScanner, TypeAnnotationsScanner }
+import org.reflections.util.{ ClasspathHelper, ConfigurationBuilder }
+import org.scalatest.{ Matchers, WordSpec }
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable
@@ -18,7 +21,19 @@ class ApiMayChangeDocCheckerSpec extends WordSpec with Matchers {
     clazz.getCanonicalName.replaceAll("\\$minus", "-").split("\\$")(0)
   }
 
-  "ApiMayChange Docs" should {
+  // As Specs, Directives and HttpApp inherit get all directives methods, we skip those as they are not really bringing any extra info
+  def removeClassesToIgnore(method: Method): Boolean = {
+    Seq("Spec", ".Directives", ".HttpApp").exists(method.getDeclaringClass.getCanonicalName.contains)
+  }
+
+  def collectMissing(docPage: Seq[String])(set: Set[String], name: String): Set[String] = {
+    if (docPage.exists(line => line.contains(name)))
+      set
+    else
+      set + name
+  }
+
+  "compatibility-guidelines.md doc page" should {
     val reflections = new Reflections(new ConfigurationBuilder()
       .setUrls(ClasspathHelper.forPackage("akka.http"))
       .setScanners(
@@ -27,33 +42,19 @@ class ApiMayChangeDocCheckerSpec extends WordSpec with Matchers {
     val docPage = Source.fromFile("docs/src/main/paradox/compatibility-guidelines.md").getLines().toList
     "contain all ApiMayChange references in classes" in {
       val classes: mutable.Set[Class[_]] = reflections.getTypesAnnotatedWith(classOf[ApiMayChange], true).asScala
-      val missing = classes.foldLeft(Set.empty[String])((set, clazz) => {
-        val prettyName = prettifyName(clazz)
-        if (docPage.exists(line => line.contains(prettyName)))
-          set
-        else
-          set + prettyName
-      })
+      val missing = classes
+        .map(prettifyName)
+        .foldLeft(Set.empty[String])(collectMissing(docPage))
       missing shouldBe Set.empty
     }
     "contain all ApiMayChange references in methods" in {
       val methods = reflections.getMethodsAnnotatedWith(classOf[ApiMayChange]).asScala
       val missing = methods
         .filterNot(removeClassesToIgnore)
-        .foldLeft(Set.empty[String])((set, method) => {
-          val prettyName = prettifyName(method.getDeclaringClass) + "#" + method.getName
-          if (docPage.exists(line => line.contains(prettyName)))
-            set
-          else
-            set + prettyName
-        })
+        .map(method => prettifyName(method.getDeclaringClass) + "#" + method.getName)
+        .foldLeft(Set.empty[String])(collectMissing(docPage))
       missing shouldBe Set.empty
     }
 
-  }
-
-  // As Specs, Directives and HttpApp inherit get all directives methods, we skip those as they are not really bringing any extra info
-  def removeClassesToIgnore(method: Method) = {
-    Seq("Spec", ".Directives", ".HttpApp").exists(method.getDeclaringClass.getCanonicalName.contains)
   }
 }
