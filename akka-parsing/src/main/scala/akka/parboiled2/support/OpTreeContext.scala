cf425084c6272c9a4a75a579c463d18b79f5bfdd
sergei.winitzki <sergei.winitzki@workday.com>
2018-03-08 17:31:07 -0800

    revert change that breaks 2.11 compile

--- akka-parsing/src/main/scala/akka/parboiled2/support/OpTreeContext.scala
+++ akka-parsing/src/main/scala/akka/parboiled2/support/OpTreeContext.scala
@@ -595,7 +595,7 @@ trait OpTreeContext[OpTreeCtx <: ParserMacros.ParserContext] {
         case Left(_)  ⇒ super.render(wrapped)
         case Right(x) ⇒ q"$x ne null"
       }
-    def renderInner(wrapped: Boolean) = call.asInstanceOf[Left[OpTree, Tree]].value.render(wrapped)
+    def renderInner(wrapped: Boolean) = call.asInstanceOf[Left[OpTree, Tree]].a.render(wrapped)
   }
 
   def CharRange(lowerTree: Tree, upperTree: Tree): CharacterRange = {
