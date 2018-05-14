5ab7dd9d24e081c4bb1fd86609f14f55f4166bad
Arnout Engelen <arnouten@bzzt.net>
2018-05-14 10:04:26 +0200

    Remove 'require' from benchmark

--- akka-http-bench-jmh/src/main/scala/akka/http/impl/engine/HeaderParserBenchmark.scala
+++ akka-http-bench-jmh/src/main/scala/akka/http/impl/engine/HeaderParserBenchmark.scala
@@ -58,8 +58,6 @@ class HeaderParserBenchmark {
   @Benchmark
   def bench_parse_headers(): Int = {
     val next = parser.parseHeaderLine(requestBytes, firstHeaderStart)()
-    val last = parser.parseHeaderLine(requestBytes, next)()
-    require(last == 84, s"Expected to read until 84, but read until $last")
-    last
+    parser.parseHeaderLine(requestBytes, next)()
   }
 }
