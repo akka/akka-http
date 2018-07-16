21cdec347461b4d8d022300c1f77c5378e680e67
was1wa3 <stefan.wachter@bosch-si.com>
2018-07-16 16:23:28 +0200

    make RouteWithConcatenation a value class; make argument of '~' method by name

--- akka-http/src/main/scala/akka/http/scaladsl/server/RouteConcatenation.scala
+++ akka-http/src/main/scala/akka/http/scaladsl/server/RouteConcatenation.scala
@@ -34,12 +34,12 @@ trait RouteConcatenation {
 
 object RouteConcatenation extends RouteConcatenation {
 
-  class RouteWithConcatenation(route: Route) {
+  class RouteWithConcatenation(val route: Route) extends AnyVal {
     /**
      * Returns a Route that chains two Routes. If the first Route rejects the request the second route is given a
      * chance to act upon the request.
      */
-    def ~(other: Route): Route = { ctx ⇒
+    def ~(other: ⇒ Route): Route = { ctx ⇒
       import ctx.executionContext
       route(ctx).fast.flatMap {
         case x: RouteResult.Complete ⇒ FastFuture.successful(x)
