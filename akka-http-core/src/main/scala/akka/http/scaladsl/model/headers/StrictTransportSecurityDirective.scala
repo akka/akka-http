91ea0681da20caf5fa7f65123ffe6b85801a8141
Michal Sindlar <m.sindlar@portavita.eu>
2018-07-27 11:10:29 +0200

    Mark StrictTransportSecurityDirective @DoNoInherit

--- akka-http-core/src/main/scala/akka/http/scaladsl/model/headers/StrictTransportSecurityDirective.scala
+++ akka-http-core/src/main/scala/akka/http/scaladsl/model/headers/StrictTransportSecurityDirective.scala
@@ -4,6 +4,10 @@
 
 package akka.http.scaladsl.model.headers
 
+import akka.annotation.DoNotInherit
+
+/** Not for user extension */
+@DoNotInherit
 sealed abstract class StrictTransportSecurityDirective
 final case class IgnoredDirective(value: String) extends StrictTransportSecurityDirective
 case object IncludeSubDomains extends StrictTransportSecurityDirective
