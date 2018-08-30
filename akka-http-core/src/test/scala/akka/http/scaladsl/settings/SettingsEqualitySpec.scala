df3b682848b3844b8cde92e96791f9b610f62e6e
Arnout Engelen <arnouten@bzzt.net>
2018-08-30 18:14:57 +0200

    [backport from master] =htp add size limit to `decodeRequest` directives, ref #2137

--- akka-http-core/src/test/scala/akka/http/scaladsl/settings/SettingsEqualitySpec.scala
+++ akka-http-core/src/test/scala/akka/http/scaladsl/settings/SettingsEqualitySpec.scala
@@ -19,6 +19,7 @@ class SettingsEqualitySpec extends WordSpec with Matchers {
       range-coalescing-threshold = 80
       range-count-limit = 16
       decode-max-bytes-per-chunk = 1m
+      decode-max-size = 8m
       file-io-dispatcher = ${akka.stream.blocking-io-dispatcher}
     }
   """).withFallback(ConfigFactory.load).resolve
