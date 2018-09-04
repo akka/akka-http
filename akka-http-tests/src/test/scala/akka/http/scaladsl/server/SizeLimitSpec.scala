58aed5392b1eb743789e662339b5edf944fdae2c
Arnout Engelen <arnouten@bzzt.net>
2018-09-04 15:55:09 +0200

    Removed outdated comment

--- akka-http-tests/src/test/scala/akka/http/scaladsl/server/SizeLimitSpec.scala
+++ akka-http-tests/src/test/scala/akka/http/scaladsl/server/SizeLimitSpec.scala
@@ -61,9 +61,6 @@ class SizeLimitSpec extends WordSpec with Matchers with RequestBuilding with Bef
     }
 
     "not accept entities bigger than configured with akka.http.parsing.max-content-length" in {
-      // It went from 1 occurrence to 2 after discarding the entity, I think is due to the retrying nature of `handleRejections`
-      // that causes one Exception for the original entity and another one from the rejected one.
-      // Then from 2 to 4 since the change to add a `Limitable` whenever `withSizeLimit` is applied
       Http().singleRequest(Post(s"http:/${binding.localAddress}/noDirective", entityOfSize(maxContentLength + 1)))
         .futureValue.status shouldEqual StatusCodes.BadRequest
     }
