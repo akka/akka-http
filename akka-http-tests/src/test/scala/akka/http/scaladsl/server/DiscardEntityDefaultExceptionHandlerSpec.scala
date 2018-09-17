1f293623e43fbaebcc4d41a16e8ac454add5d342
Josep Prat <jlprat@gmail.com>
2018-09-18 01:23:14 +0200

    Add test case when the entity is completely consumed when the exception is thrown

--- akka-http-tests/src/test/scala/akka/http/scaladsl/server/DiscardEntityDefaultExceptionHandlerSpec.scala
+++ akka-http-tests/src/test/scala/akka/http/scaladsl/server/DiscardEntityDefaultExceptionHandlerSpec.scala
@@ -12,11 +12,20 @@ import akka.util.ByteString
 import org.scalatest.concurrent.Eventually._
 import org.scalatest.concurrent.ScalaFutures
 
+import scala.concurrent.Future
+
 class DiscardEntityDefaultExceptionHandlerSpec extends RoutingSpec with ScalaFutures {
 
-  private val route = path("crash") {
-    throw new RuntimeException("BOOM!")
-  }
+  private val route = concat(
+    path("crash") {
+      throw new RuntimeException("BOOM!")
+    }, path("crashAfterConsuming") {
+      extractRequestEntity { entity ⇒
+        val future: Future[String] = entity.dataBytes.runFold(ByteString.empty)(_ ++ _).map(_ ⇒ throw new RuntimeException("KABOOM!"))
+        complete(future)
+      }
+    }
+  )
 
   private val numElems = 1000
   @volatile
@@ -31,6 +40,14 @@ class DiscardEntityDefaultExceptionHandlerSpec extends RoutingSpec with ScalaFut
 
   "Default ExceptionHandler" should {
     "rejectEntity by default" in {
+      RequestToCrash ~> Route.seal(route) ~> check {
+        status shouldBe InternalServerError
+        eventually {
+          elementsEmitted shouldBe numElems
+        }
+      }
+    }
+    "rejectEntity by default even if consumed already" in {
       RequestToCrash ~> Route.seal(route) ~> check {
         status shouldBe InternalServerError
         eventually {
