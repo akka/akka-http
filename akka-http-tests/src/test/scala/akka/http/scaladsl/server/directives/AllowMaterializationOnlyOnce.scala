6d9816786cfcfef9d7b067580a3c2294c3dfb3f4
Arnout Engelen <arnouten@bzzt.net>
2018-11-20 14:58:25 +0100

    Reproducer for formFields error on streamed request

---
+++ akka-http-tests/src/test/scala/akka/http/scaladsl/server/directives/AllowMaterializationOnlyOnce.scala
@@ -0,0 +1,19 @@
+package akka.http.scaladsl.server.directives
+
+import java.util.concurrent.atomic.AtomicBoolean
+
+import akka.NotUsed
+import akka.stream.scaladsl.Flow
+
+object AllowMaterializationOnlyOnce {
+  def apply[T, Mat](): Flow[T, T, NotUsed] = {
+    val materialized = new AtomicBoolean(false)
+    Flow[T].mapMaterializedValue { mat =>
+      if (materialized.compareAndSet(false, true)) {
+        mat
+      } else {
+        throw new IllegalStateException("Substream Source cannot be materialized more than once")
+      }
+    }
+  }
+}
