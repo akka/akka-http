8a95df84dcdda114aecdcc4c8f7e8987f31ad947
Josep Prat <jlprat@gmail.com>
2018-12-03 21:44:57 +0100

    =doc Add Route.seal example to Exception Handling docs #266
    Add Route.seal example to Exception Handling docs
    Removes extra `implicit` in example where it wasn't needed

--- docs/src/test/java/docs/http/javadsl/ExceptionHandlerInSealExample.java
+++ docs/src/test/java/docs/http/javadsl/ExceptionHandlerInSealExample.java
@@ -7,6 +7,7 @@ package docs.http.javadsl;
 import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.ConnectHttp;
+import akka.http.javadsl.Http;
 import akka.http.javadsl.ServerBinding;
 import akka.http.javadsl.model.HttpRequest;
 import akka.http.javadsl.model.HttpResponse;
@@ -14,23 +15,23 @@ import akka.http.javadsl.model.StatusCodes;
 import akka.http.javadsl.server.AllDirectives;
 import akka.http.javadsl.server.ExceptionHandler;
 import akka.http.javadsl.server.PathMatchers;
+import akka.http.javadsl.server.RejectionHandler;
 import akka.http.javadsl.server.Route;
-import akka.http.javadsl.Http;
 import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 
-import java.io.IOException;
 import java.util.concurrent.CompletionStage;
+
 import static akka.http.javadsl.server.PathMatchers.integerSegment;
 
-//#explicit-handler-example
-public class ExceptionHandlerExample extends AllDirectives {
+//#seal-handler-example
+public class ExceptionHandlerInSealExample extends AllDirectives {
   public static void main(String[] args) {
     final ActorSystem system = ActorSystem.create();
     final ActorMaterializer materializer = ActorMaterializer.create(system);
     final Http http = Http.get(system);
 
-    final ExceptionHandlerExample app = new ExceptionHandlerExample();
+    final ExceptionHandlerInSealExample app = new ExceptionHandlerInSealExample();
 
     final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = app.createRoute().flow(system, materializer);
     final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow, ConnectHttp.toHost("localhost", 8080), materializer);
@@ -43,9 +44,11 @@ public class ExceptionHandlerExample extends AllDirectives {
         complete(StatusCodes.BAD_REQUEST, "You've got your arithmetic wrong, fool!"))
       .build();
 
+    final RejectionHandler defaultHandler = RejectionHandler.defaultHandler();
+
     return path(PathMatchers.segment("divide").slash(integerSegment()).slash(integerSegment()), (a, b) ->
-      handleExceptions(divByZeroHandler, () -> complete("The result is " + (a / b)))
-    );
+      complete("The result is " + (a / b))
+    ).seal(defaultHandler, divByZeroHandler);
   }
 }
-//#explicit-handler-example
+//#seal-handler-example
