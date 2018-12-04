b150432e9bb7ef6b441fda527dc0a6ec42797fad
Arnout Engelen <arnout@bzzt.net>
2018-12-04 18:26:50 +0100

    Add ccompat for scala 2.11

---
+++ akka-http-core/src/main/scala-2.11/akka/http/ccompat/MapHelpers.scala
@@ -0,0 +1,8 @@
+package akka.http.ccompat
+
+object MapHelpers {
+  def convertMapToScala[K, V](jmap: java.util.Map[K, V]): scala.collection.immutable.Map[K, V] = {
+    import scala.collection.JavaConverters._
+    Map.empty ++ jmap.asScala
+  }
+}
---
+++ akka-http-core/src/main/scala-2.11/akka/http/ccompat/package.scala
@@ -0,0 +1,29 @@
+package akka.http
+
+import scala.collection.mutable
+
+package object ccompat {
+
+  /**
+   * On scala 2.12, we often provide the same method both in 'immutable.Seq' and 'varargs' variation.
+   *
+   * On scala 2.13, this is no longer necessary, since varargs are treated as immutable. Therefore we need
+   * to use a specific name so we can disambiguate the 2 methods for 2.13:
+   */
+  type VASeq[+A] = scala.collection.immutable.Seq[A]
+  implicit class ConvertableVASeq[A](val seq: VASeq[A]) extends AnyVal {
+    def xSeq: scala.collection.immutable.Seq[A] = seq
+  }
+
+  trait Builder[-Elem, +To] extends mutable.Builder[Elem, To] { self ⇒
+    // This became final in 2.13 so cannot be overridden there anymore
+    final override def +=(elem: Elem): this.type = addOne(elem)
+    def addOne(elem: Elem): this.type = self.+=(elem)
+  }
+
+  trait QuerySeqOptimized extends scala.collection.immutable.LinearSeq[(String, String)] with scala.collection.LinearSeqOptimized[(String, String), akka.http.scaladsl.model.Uri.Query] {
+    self: akka.http.scaladsl.model.Uri.Query ⇒
+    override def newBuilder: mutable.Builder[(String, String), akka.http.scaladsl.model.Uri.Query] = akka.http.scaladsl.model.Uri.Query.newBuilder
+  }
+
+}
