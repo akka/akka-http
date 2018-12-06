30ddb8b9f6b95cac5247a8f6dfd0686e3ee6df1e
Renato  Cavalcanti <renato@cavalcanti.be>
2018-12-06 16:16:14 +0100

    move RoutingSettings to akka-http module

---
+++ akka-http-tests/src/test/scala/akka/http/scaladsl/settings/RoutingSettingsEqualitySpec.scala
@@ -0,0 +1,22 @@
+package akka.http.scaladsl.settings
+
+import com.typesafe.config.ConfigFactory
+import org.scalatest.{ Matchers, WordSpec }
+
+class RoutingSettingsEqualitySpec extends WordSpec with Matchers {
+
+  val config = ConfigFactory.load.resolve
+
+  "equality" should {
+
+    "hold for RoutingSettings" in {
+      val s1 = RoutingSettings(config)
+      val s2 = RoutingSettings(config)
+
+      s1 shouldBe s2
+      s1.toString should startWith("RoutingSettings(")
+    }
+
+  }
+
+}
