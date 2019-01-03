269b66b0e936ab9592f7c90084f42af4fa030fa4
Hungai Amuhinda <hungaiinc@gmail.com>
2019-01-03 17:46:41 +0300

    Bump Year

--- project/CopyrightHeader.scala
+++ project/CopyrightHeader.scala
@@ -33,7 +33,7 @@ object CopyrightHeader extends AutoPlugin {
 
   // We hard-code this so PR's created in year X will not suddenly fail in X+1.
   // Of course we should remember to update it early in the year.
-  val CurrentYear = "2018"
+  val CurrentYear = "2019"
   val CopyrightPattern = "Copyright \\([Cc]\\) (\\d{4}(-\\d{4})?) (Lightbend|Typesafe) Inc. <.*>".r
   val CopyrightHeaderPattern = s"(?s).*${CopyrightPattern}.*".r
 
