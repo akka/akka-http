55a09ba85b2fe4f0f13e65a20d2504179005b173
Arnout Engelen <arnout@bzzt.net>
2019-01-04 15:52:25 +0100

    make akka-http2-support compile
    not test:akka-http2-support yet as that needs akka-http first

---
+++ akka-http-core/src/main/scala-2.13.0-M5/akka/http/ccompat/imm/package.scala
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2018 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.http.ccompat
+
+import scala.collection.immutable
+
+/**
+ * INTERNAL API
+ */
+package object imm {
+  // Nothing yet, but present to be source-compatible with 2.13-
+}
