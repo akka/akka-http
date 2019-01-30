1bc3759ba4e16d5482a81ae36f1438b08b94d635
Nathan Fischer <nfischer921@gmail.com>
2019-01-29 17:51:52 -0800

    Add to directive chain
    fix 2.11 compile issues

--- akka-http/src/main/scala/akka/http/javadsl/server/directives/ReverseProxyDirectives.scala
+++ akka-http/src/main/scala/akka/http/javadsl/server/directives/ReverseProxyDirectives.scala
@@ -3,25 +3,33 @@ package akka.http.javadsl.server.directives
 import java.util.concurrent.CompletionStage
 import java.util.function.{ Function ⇒ JFunction }
 
-import akka.http.javadsl.model.{ HttpRequest, HttpResponse, Uri }
+import akka.http.javadsl.model.{ Uri, HttpRequest ⇒ JHttpRequest, HttpResponse ⇒ JHttpResponse }
 import akka.http.javadsl.server.Route
+import akka.http.scaladsl.model
+import akka.http.scaladsl.model.HttpResponse
 import akka.http.scaladsl.server.directives.ReverseProxyDirectives.{ ReverseProxyTargetConfig, ReverseProxyTargetMagnet }
 import akka.http.scaladsl.server.{ Directives ⇒ D }
-import scala.compat.java8.FutureConverters._
 
-abstract class ReverseProxyDirectives {
+import scala.compat.java8.FunctionConverters._
+import scala.compat.java8.FutureConverters._
+import scala.concurrent.Future
+
+abstract class ReverseProxyDirectives extends FramedEntityStreamingDirectives {
   import akka.http.impl.util.JavaMapping.Implicits._
+
+  private val javaToScalaResponse = ((res: JHttpResponse) ⇒ res.asScala).asJava
 
   def reverseProxy(
     targetUri:        Uri,
     useUnmatchedPath: Boolean,
-    httpClient:       JFunction[HttpRequest, CompletionStage[HttpResponse]]
+    httpClient:       JFunction[JHttpRequest, CompletionStage[JHttpResponse]]
   ): Route = {
     def _client = httpClient
     RouteAdapter(
       D.reverseProxy(new ReverseProxyTargetMagnet {
         val config = ReverseProxyTargetConfig(targetUri.asScala, useUnmatchedPath)
-        val httpClient = req ⇒ _client(req).thenApply[akka.http.scaladsl.model.HttpResponse](res ⇒ res.asScala).toScala
+        val httpClient: model.HttpRequest ⇒ Future[model.HttpResponse] =
+          req ⇒ _client(req).thenApply[HttpResponse](javaToScalaResponse).toScala
       })
     )
   }
