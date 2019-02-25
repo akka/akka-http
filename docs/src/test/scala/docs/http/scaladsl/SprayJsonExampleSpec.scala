3696590a7bd6ec3cf25221babd133e121990e852
Vitalii Lysov <vitalii.lysov@gmail.com>
2019-02-25 13:31:08 +0100

    make code example in introduction doc self-contained
    This code snippet is referenced in the introduction documentation of akka-http:
    https://doc.akka.io/docs/akka-http/current/introduction.html
    It was a frustrating experience (again) with examples from the doc. I had to spend some time to find out why the code snippet didn't compile. Mentioning this required dependency for this snippet would save me and others time.

--- docs/src/test/scala/docs/http/scaladsl/SprayJsonExampleSpec.scala
+++ docs/src/test/scala/docs/http/scaladsl/SprayJsonExampleSpec.scala
@@ -57,6 +57,8 @@ class SprayJsonExampleSpec extends WordSpec with Matchers {
     import akka.http.scaladsl.server.Route
     import akka.http.scaladsl.server.Directives._
     import akka.http.scaladsl.model.StatusCodes
+    // for JSON serialization/deserialization following dependency is required:
+    // "com.typesafe.akka" %% "akka-http-spray-json" % "10.1.7"
     import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
     import spray.json.DefaultJsonProtocol._
 
