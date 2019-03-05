ae0720d5048fde3aa1395b1c1b56d709587bded1
Johannes Rudolph <johannes.rudolph@gmail.com>
2019-03-05 11:18:45 +0100

    Update to latest scalatest 3.0.6 (#2445)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -27,7 +27,7 @@ object Dependencies {
     crossScalaVersions := Seq("2.12.8", "2.11.12", "2.13.0-M5"),
     scalaVersion := crossScalaVersions.value.head,
     scalaCheckVersion := System.getProperty("akka.build.scalaCheckVersion", "1.14.0"),
-    scalaTestVersion := "3.0.6-SNAP6",
+    scalaTestVersion := "3.0.6",
     specs2Version := "4.3.6"
   )
 
