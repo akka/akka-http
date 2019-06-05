33cbb50a43cbe18a3d76178516121aa54b4dce19
Johannes Rudolph <johannes.rudolph@gmail.com>
2019-06-05 12:25:36 +0200

    htp: refactor SecurityDirectiveSpecs

--- akka-http-tests/src/test/scala/akka/http/scaladsl/server/directives/SecurityDirectivesSpec.scala
+++ akka-http-tests/src/test/scala/akka/http/scaladsl/server/directives/SecurityDirectivesSpec.scala
@@ -12,133 +12,80 @@ import akka.http.scaladsl.server.AuthenticationFailedRejection.{ CredentialsReje
 import akka.testkit.EventFilter
 
 class SecurityDirectivesSpec extends RoutingSpec {
-  val dontBasicAuth = authenticateBasicAsync[String]("MyRealm", _ => Future.successful(None))
-  val dontOAuth2Auth = authenticateOAuth2Async[String]("MyRealm", _ => Future.successful(None))
   val doBasicAuth = authenticateBasicPF("MyRealm", { case Credentials.Provided(identifier) => identifier })
-  val doOAuth2Auth = authenticateOAuth2PF("MyRealm", { case Credentials.Provided(identifier) => identifier })
-  val authWithAnonymous = doBasicAuth.withAnonymousUser("We are Legion")
-
+  val dontBasicAuth = authenticateBasicAsync[String]("MyRealm", _ => Future.successful(None))
   val basicChallenge = HttpChallenges.basic("MyRealm")
+
+  val doOAuth2Auth = authenticateOAuth2PF("MyRealm", { case Credentials.Provided(identifier) => identifier })
+  val dontOAuth2Auth = authenticateOAuth2Async[String]("MyRealm", _ => Future.successful(None))
   val oAuth2Challenge = HttpChallenges.oAuth2("MyRealm")
 
-  "basic authentication" should {
-    "reject requests without Authorization header with an AuthenticationFailedRejection" in {
-      Get() ~> {
-        dontBasicAuth { echoComplete }
-      } ~> check { rejection shouldEqual AuthenticationFailedRejection(CredentialsMissing, basicChallenge) }
-    }
-    "reject unauthenticated requests with Authorization header with an AuthenticationFailedRejection" in {
-      Get() ~> Authorization(BasicHttpCredentials("Bob", "")) ~> {
-        dontBasicAuth { echoComplete }
-      } ~> check { rejection shouldEqual AuthenticationFailedRejection(CredentialsRejected, basicChallenge) }
-    }
-    "reject requests with an OAuth2 Bearer Token Authorization header with 401" in {
-      Get() ~> Authorization(OAuth2BearerToken("myToken")) ~> Route.seal {
-        dontOAuth2Auth { echoComplete }
-      } ~> check {
-        status shouldEqual StatusCodes.Unauthorized
-        responseAs[String] shouldEqual "The supplied authentication is invalid"
-        header[`WWW-Authenticate`] shouldEqual Some(`WWW-Authenticate`(oAuth2Challenge))
+  authenticationTests("Basic", doBasicAuth, dontBasicAuth, basicChallenge, BasicHttpCredentials(_, ""))
+  authenticationTests("OAuth2 Bearer token", doOAuth2Auth, dontOAuth2Auth, oAuth2Challenge, OAuth2BearerToken(_))
+
+  def authenticationTests(
+    authenticationType:      String,
+    successfulAuthDirective: AuthenticationDirective[String],
+    failedAuthDirective:     AuthenticationDirective[String],
+    challenge:               HttpChallenge,
+    createCredentials:       String => HttpCredentials
+  ): Unit =
+    s"$authenticationType authentication" should {
+      "reject requests without Authorization header with an AuthenticationFailedRejection" in {
+        Get() ~> {
+          failedAuthDirective { echoComplete }
+        } ~> check { rejection shouldEqual AuthenticationFailedRejection(CredentialsMissing, challenge) }
+      }
+      "reject unauthenticated requests with Authorization header with an AuthenticationFailedRejection" in {
+        Get() ~> Authorization(createCredentials("Bob")) ~> {
+          failedAuthDirective { echoComplete }
+        } ~> check { rejection shouldEqual AuthenticationFailedRejection(CredentialsRejected, challenge) }
+      }
+      "reject requests with an any other credentials in Authorization header with 401" in {
+        Get() ~> Authorization(GenericHttpCredentials("Other", "token")) ~> Route.seal {
+          successfulAuthDirective { echoComplete }
+        } ~> check {
+          status shouldEqual StatusCodes.Unauthorized
+          responseAs[String] shouldEqual "The resource requires authentication, which was not supplied with the request"
+          header[`WWW-Authenticate`] shouldEqual Some(`WWW-Authenticate`(challenge))
+        }
+      }
+      "reject requests with illegal Authorization header with 401" in {
+        Get() ~> RawHeader("Authorization", "bob alice") ~> Route.seal {
+          failedAuthDirective { echoComplete }
+        } ~> check {
+          status shouldEqual StatusCodes.Unauthorized
+          responseAs[String] shouldEqual "The resource requires authentication, which was not supplied with the request"
+          header[`WWW-Authenticate`] shouldEqual Some(`WWW-Authenticate`(challenge))
+        }
+      }
+      "extract the object representing the user identity created by successful authentication" in {
+        Get() ~> Authorization(createCredentials("Alice")) ~> {
+          successfulAuthDirective { echoComplete }
+        } ~> check { responseAs[String] shouldEqual "Alice" }
+      }
+      "extract the object representing the user identity created for the anonymous user" in {
+        Get() ~> {
+          successfulAuthDirective.withAnonymousUser("We are Legion") { echoComplete }
+        } ~> check { responseAs[String] shouldEqual "We are Legion" }
+      }
+      "handle exceptions thrown in its inner route" in {
+        object TestException extends RuntimeException("Boom")
+        EventFilter[TestException.type](
+          occurrences = 1,
+          start = "Error during processing of request: 'Boom'. Completing with 500 Internal Server Error response."
+        ).intercept {
+          Get() ~> Authorization(createCredentials("Alice")) ~> {
+            Route.seal {
+              successfulAuthDirective { _ => throw TestException }
+            }
+          } ~> check { status shouldEqual StatusCodes.InternalServerError }
+        }
       }
     }
-    "reject requests with illegal Authorization header with 401" in {
-      Get() ~> RawHeader("Authorization", "bob alice") ~> Route.seal {
-        dontBasicAuth { echoComplete }
-      } ~> check {
-        status shouldEqual StatusCodes.Unauthorized
-        responseAs[String] shouldEqual "The resource requires authentication, which was not supplied with the request"
-        header[`WWW-Authenticate`] shouldEqual Some(`WWW-Authenticate`(basicChallenge))
-      }
-    }
-    "extract the object representing the user identity created by successful authentication" in {
-      Get() ~> Authorization(BasicHttpCredentials("Alice", "")) ~> {
-        doBasicAuth { echoComplete }
-      } ~> check { responseAs[String] shouldEqual "Alice" }
-    }
-    "extract the object representing the user identity created for the anonymous user" in {
-      Get() ~> {
-        authWithAnonymous { echoComplete }
-      } ~> check { responseAs[String] shouldEqual "We are Legion" }
-    }
-    "properly handle exceptions thrown in its inner route" in {
-      object TestException extends RuntimeException("Boom")
-      EventFilter[TestException.type](
-        occurrences = 1,
-        start = "Error during processing of request: 'Boom'. Completing with 500 Internal Server Error response."
-      ).intercept {
-        Get() ~> Authorization(BasicHttpCredentials("Alice", "")) ~> {
-          Route.seal {
-            doBasicAuth { _ => throw TestException }
-          }
-        } ~> check { status shouldEqual StatusCodes.InternalServerError }
-      }
-    }
-  }
-  "bearer token authentication" should {
-    "reject requests without Authorization header with an AuthenticationFailedRejection" in {
-      Get() ~> {
-        dontOAuth2Auth { echoComplete }
-      } ~> check { rejection shouldEqual AuthenticationFailedRejection(CredentialsMissing, oAuth2Challenge) }
-    }
-    "reject unauthenticated requests with Authorization header with an AuthenticationFailedRejection" in {
-      Get() ~> Authorization(OAuth2BearerToken("myToken")) ~> {
-        dontOAuth2Auth { echoComplete }
-      } ~> check { rejection shouldEqual AuthenticationFailedRejection(CredentialsRejected, oAuth2Challenge) }
-    }
-    "reject unauthenticated requests without Authorization header but with access_token URI parameter with an AuthenticationFailedRejection" in {
-      Get("?access_token=myToken") ~> {
-        dontOAuth2Auth { echoComplete }
-      } ~> check { rejection shouldEqual AuthenticationFailedRejection(CredentialsRejected, oAuth2Challenge) }
-    }
-    "reject requests with a Basic Authorization header with 401" in {
-      Get() ~> Authorization(BasicHttpCredentials("Alice", "")) ~> Route.seal {
-        dontBasicAuth { echoComplete }
-      } ~> check {
-        status shouldEqual StatusCodes.Unauthorized
-        responseAs[String] shouldEqual "The supplied authentication is invalid"
-        header[`WWW-Authenticate`] shouldEqual Some(`WWW-Authenticate`(basicChallenge))
-      }
-    }
-    "reject requests with illegal Authorization header with 401" in {
-      Get() ~> RawHeader("Authorization", "bob alice") ~> Route.seal {
-        dontOAuth2Auth { echoComplete }
-      } ~> check {
-        status shouldEqual StatusCodes.Unauthorized
-        responseAs[String] shouldEqual "The resource requires authentication, which was not supplied with the request"
-        header[`WWW-Authenticate`] shouldEqual Some(`WWW-Authenticate`(oAuth2Challenge))
-      }
-    }
-    "extract the object representing the user identity created by successful authentication with Authorization header" in {
-      Get() ~> Authorization(OAuth2BearerToken("myToken")) ~> {
-        doOAuth2Auth { echoComplete }
-      } ~> check { responseAs[String] shouldEqual "myToken" }
-    }
-    "extract the object representing the user identity created by successful authentication with access_token URI parameter" in {
-      Get("?access_token=myToken") ~> {
-        doOAuth2Auth { echoComplete }
-      } ~> check { responseAs[String] shouldEqual "myToken" }
-    }
-    "extract the object representing the user identity created for the anonymous user" in {
-      Get() ~> {
-        authWithAnonymous { echoComplete }
-      } ~> check { responseAs[String] shouldEqual "We are Legion" }
-    }
-    "properly handle exceptions thrown in its inner route" in {
-      object TestException extends RuntimeException("Boom")
-      EventFilter[TestException.type](
-        occurrences = 1,
-        start = "Error during processing of request: 'Boom'. Completing with 500 Internal Server Error response."
-      ).intercept {
-        Get() ~> Authorization(OAuth2BearerToken("myToken")) ~> {
-          Route.seal {
-            doOAuth2Auth { _ => throw TestException }
-          }
-        } ~> check { status shouldEqual StatusCodes.InternalServerError }
-      }
-    }
-  }
+
   "authentication directives" should {
-    "properly stack" in {
+    "stack" in {
       val otherChallenge = HttpChallenge("MyAuth", Some("MyRealm2"))
       val otherAuth: Directive1[String] = authenticateOrRejectWithChallenge { (cred: Option[HttpCredentials]) =>
         Future.successful(Left(otherChallenge))
