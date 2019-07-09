ac00b591668e2b7ed33e7f6804a8b5102c4b1786
Arnout Engelen <arnout@bzzt.net>
2019-07-09 16:18:19 +0200

    Check bincompat against 10.1.8 for Scala 2.13

--- project/MiMa.scala
+++ project/MiMa.scala
@@ -60,6 +60,7 @@ object MiMa extends AutoPlugin {
       val post213Versions = Set(
           // To be updated once we have released
           // for 2.13.0
+          "10.1.8",
       )
 
       val versions =
