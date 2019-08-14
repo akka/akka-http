akka/akka-http

commit f47a6468096cca594572664f4b0b73abe6585303

core: log error when response entity stream fails
Refs #848

akka-http-core/src/main/scala/akka/http/impl/engine/server/HttpServerBluePrint.scala
@@ -414,9 +414,18 @@ private[http] object HttpServerBluePrint {
 
       setHandler(httpResponseIn, new InHandler {
         def onPush(): Unit = {
-          val response = grab(httpResponseIn)
           val requestStart = openRequests.head
           openRequests = openRequests.tail
+
+          val response0 = grab(httpResponseIn)
+          val response = response0.mapEntity { e =>
+            val (newEntity, fut) = HttpEntity.captureTermination(e)
+            fut.failed.foreach { ex =>
+              log.error(ex, s"Response stream for [${requestStart.debugString}] failed with '${ex.getMessage}'. Aborting connection.")
+            }(materializer.executionContext)
+            newEntity
+          }
+
           val isEarlyResponse = messageEndPending && openRequests.isEmpty
           if (isEarlyResponse && response.status.isSuccess)
             log.warning(

akka-http-core/src/main/scala/akka/http/impl/util/StreamUtils.scala
@@ -179,9 +179,11 @@ private[http] object StreamUtils {
       override def onDownstreamFinish(): Unit = {
         cancelAfter match {
           case finite: FiniteDuration =>
+            log.debug(s"Delaying cancellation for $finite")
             timeout = OptionVal.Some {
               scheduleOnce(finite) {
                 log.debug(s"Stage was canceled after delay of $cancelAfter")
+                timeout = OptionVal.None
                 completeStage()
               }
             }

akka-http-core/src/main/scala/akka/http/impl/util/package.scala
@@ -18,6 +18,7 @@ import scala.util.{ Failure, Success }
 import scala.util.matching.Regex
 import akka.util.ByteString
 import akka.actor._
+import akka.http.impl.engine.parsing.ParserOutput.RequestStart
 import akka.http.scaladsl.model.{ HttpEntity, HttpRequest, HttpResponse }
 
 package object util {
@@ -76,6 +77,9 @@ package object util {
 
   private[http] implicit class RichHttpRequest(val request: HttpRequest) extends AnyVal {
     def debugString: String = s"${request.method.value} ${request.uri.path} ${entityDebugInfo(request.entity)}"
+  }
+  private[http] implicit class RichRequestStart(val request: RequestStart) extends AnyVal {
+    def debugString: String = s"${request.method.value} ${request.uri.path}"
   }
   private[http] implicit class RichHttpResponse(val response: HttpResponse) extends AnyVal {
     def debugString: String = s"${response.status.value} ${entityDebugInfo(response.entity)}"

akka-http-core/src/main/scala/akka/http/scaladsl/settings/ServerSettings.scala
@@ -116,6 +116,7 @@ abstract class ServerSettings private[akka] () extends akka.http.javadsl.setting
   def mapParserSettings(f: ParserSettings => ParserSettings): ServerSettings = withParserSettings(f(parserSettings))
   def mapPreviewServerSettings(f: PreviewServerSettings => PreviewServerSettings): ServerSettings = withPreviewServerSettings(f(previewServerSettings))
   def mapWebsocketSettings(f: WebSocketSettings => WebSocketSettings): ServerSettings = withWebsocketSettings(f(websocketSettings))
+  def mapTimeouts(f: ServerSettings.Timeouts => ServerSettings.Timeouts): ServerSettings = withTimeouts(f(timeouts))
 }
 
 object ServerSettings extends SettingsCompanion[ServerSettings] {

akka-http-core/src/test/scala/akka/http/impl/engine/client/ConnectionPoolSpec.scala
@@ -10,6 +10,7 @@ import java.nio.channels.{ ServerSocketChannel, SocketChannel }
 import java.util.concurrent.atomic.AtomicInteger
 
 import akka.actor.ActorSystem
+import akka.event.Logging
 import akka.http.impl.engine.client.PoolMasterActor.PoolInterfaceRunning
 import akka.http.impl.engine.server.ServerTerminator
 import akka.http.impl.engine.ws.ByteStringSinkProbe
@@ -20,6 +21,7 @@ import akka.http.scaladsl.model.{ HttpEntity, _ }
 import akka.http.scaladsl.model.headers._
 import akka.http.scaladsl.settings.{ ClientConnectionSettings, ConnectionPoolSettings, PoolImplementation, ServerSettings }
 import akka.http.scaladsl.{ ClientTransport, ConnectionContext, Http }
+import akka.stream.Attributes
 import akka.stream.{ ActorMaterializer, OverflowStrategy, QueueOfferResult }
 import akka.stream.TLSProtocol._
 import akka.stream.scaladsl._
@@ -199,14 +201,15 @@ abstract class ConnectionPoolSpec(poolImplementation: PoolImplementation) extend
       responses mustContainLike { case (Failure(x), 43) => x.getMessage should include(ConnectionResetByPeerMessage) }
     }
 
-    // akka-http/#416
     "surface connection-level and stream-level errors while receiving response entity" in new TestSetup(autoAccept = true) {
+
       val errorOnConnection1 = Promise[ByteString]()
 
       val crashingEntity =
         Source.fromIterator(() => Iterator.fill(10)(ByteString("abc")))
           .concat(Source.fromFuture(errorOnConnection1.future))
-          .log("test")
+          .log("response-entity-stream")
+          .addAttributes(Attributes.logLevels(Logging.InfoLevel, Logging.InfoLevel, Logging.InfoLevel))
 
       val laterHandler = Promise[(HttpRequest => Future[HttpResponse]) => Unit]()
 
@@ -239,10 +242,13 @@ abstract class ConnectionPoolSpec(poolImplementation: PoolImplementation) extend
       // ensure that server has seen request 2
       val handlerSetter = Await.result(laterHandler.future, 1.second.dilated)
 
-      // now fail the first one
-      EventFilter[RuntimeException](occurrences = 1) intercept {
-        errorOnConnection1.failure(new RuntimeException)
+      // now fail the first one, expecting the server-side error message
+      EventFilter[RuntimeException](message = "Response stream for [GET /a] failed with 'Woops, slipped on a slippery slope.'. Aborting connection.", occurrences = 1) intercept {
+        errorOnConnection1.failure(new RuntimeException("Woops, slipped on a slippery slope."))
       }
+
+      // expect "reset by peer" on the client side
+      probe1.expectError()
 
       // waiting for error to trigger connection pool failure
       Thread.sleep(2000)

akka-http-core/src/test/scala/akka/http/impl/engine/server/HttpServerSpec.scala
@@ -29,11 +29,12 @@ import scala.reflect.ClassTag
 import scala.util.Random
 
 class HttpServerSpec extends AkkaSpec(
-  """akka.loggers = []
-     akka.loglevel = OFF
+  """akka.loggers = ["akka.http.impl.util.SilenceAllTestEventListener"]
+     akka.loglevel = DEBUG
+     akka.http.server.log-unencrypted-network-bytes = 100
      akka.http.server.request-timeout = infinite
      akka.scheduler.implementation = "akka.testkit.ExplicitlyTriggeredScheduler"
-  """) with Inside { spec =>
+  """) with Inside with WithLogCapturing { spec =>
   implicit val materializer = ActorMaterializer()
 
   "The server implementation" should {
@@ -353,6 +354,8 @@ class HttpServerSpec extends AkkaSpec(
     })
 
     "close the connection if request entity stream has been cancelled" in assertAllStagesStopped(new TestSetup {
+      override def settings: ServerSettings = super.settings.mapTimeouts(_.withLingerTimeout(10.millis))
+
       // two chunks sent by client
       send("""POST / HTTP/1.1
              |Host: example.com
@@ -376,6 +379,7 @@ class HttpServerSpec extends AkkaSpec(
           dataProbe.expectNext(Chunk(ByteString("abcdef")))
           dataProbe.expectComplete()
           // connection closes once requested elements are consumed
+          scheduler.timePasses(100.millis) // > lingerTimeout to trigger delay cancellation
           netIn.expectCancellation()
       }
       shutdownBlueprint()
@@ -801,6 +805,39 @@ class HttpServerSpec extends AkkaSpec(
 
       netIn.sendComplete()
       netOut.expectComplete()
+    })
+    "log error and reset connection when the response stream fails" in assertAllStagesStopped(new TestSetup {
+      override def settings: ServerSettings = super.settings.mapTimeouts(_.withLingerTimeout(10.millis))
+
+      send("""POST /inject-meteor HTTP/1.1
+             |Host: example.com
+             |
+             |""".stripMarginWithNewline("\r\n"))
+
+      expectRequest()
+
+      val dataOutProbe = TestPublisher.probe[ByteString]()
+      val outEntity = HttpEntity.Chunked.fromData(ContentTypes.`application/octet-stream`, Source.fromPublisher(dataOutProbe))
+
+      responses.sendNext(HttpResponse(entity = outEntity))
+      expectResponseWithWipedDate(
+        """HTTP/1.1 200 OK
+          |Server: akka-http/test
+          |Date: XXXX
+          |Transfer-Encoding: chunked
+          |Content-Type: application/octet-stream
+          |
+          |""")
+      dataOutProbe.sendNext(ByteString("Hello"))
+      netOut.expectUtf8EncodedString("5\r\nHello\r\n")
+
+      EventFilter.error("Response stream for [POST /inject-meteor] failed with 'Meteor wiped data center'. Aborting connection.", occurrences = 1).intercept {
+        dataOutProbe.sendError(new RuntimeException("Meteor wiped data center"))
+      }
+
+      netOut.expectError()
+      scheduler.timePasses(100.millis) // > lingerTimeout to trigger delay cancellation
+      netIn.expectCancellation()
     })
 
     "correctly consume and render large requests and responses" in assertAllStagesStopped(new TestSetup {

akka-http-core/src/test/scala/akka/http/impl/engine/server/HttpServerTestSetupBase.scala
@@ -10,7 +10,6 @@ import akka.stream.TLSProtocol._
 
 import scala.concurrent.duration.FiniteDuration
 import akka.actor.ActorSystem
-import akka.event.NoLogging
 import akka.util.ByteString
 import akka.stream._
 import akka.stream.scaladsl._
@@ -37,7 +36,7 @@ abstract class HttpServerTestSetupBase {
     val netIn = TestPublisher.probe[ByteString]()
     val netOut = ByteStringSinkProbe()
 
-    RunnableGraph.fromGraph(GraphDSL.create(modifyServer(HttpServerBluePrint(settings, log = NoLogging, isSecureConnection = false))) { implicit b => server =>
+    RunnableGraph.fromGraph(GraphDSL.create(modifyServer(Http().serverLayer(settings))) { implicit b => server =>
       import GraphDSL.Implicits._
       Source.fromPublisher(netIn) ~> Flow[ByteString].map(SessionBytes(null, _)) ~> server.in2
       server.out1 ~> Flow[SslTlsOutbound].collect { case SendBytes(x) => x }.buffer(1, OverflowStrategy.backpressure) ~> netOut.sink

commit 294eced793664adfb50018bc156d00f54937734a

Fix performance regression introduced in f47a6468096, refs #2642

akka-http-core/src/main/scala/akka/http/impl/engine/server/HttpServerBluePrint.scala
@@ -418,13 +418,15 @@ private[http] object HttpServerBluePrint {
           openRequests = openRequests.tail
 
           val response0 = grab(httpResponseIn)
-          val response = response0.mapEntity { e =>
-            val (newEntity, fut) = HttpEntity.captureTermination(e)
-            fut.failed.foreach { ex =>
-              log.error(ex, s"Response stream for [${requestStart.debugString}] failed with '${ex.getMessage}'. Aborting connection.")
-            }(materializer.executionContext)
-            newEntity
-          }
+          val response =
+            if (response0.entity.isStrict) response0 // response stream cannot fail
+            else response0.mapEntity { e =>
+              val (newEntity, fut) = HttpEntity.captureTermination(e)
+              fut.failed.foreach { ex =>
+                log.error(ex, s"Response stream for [${requestStart.debugString}] failed with '${ex.getMessage}'. Aborting connection.")
+              }(materializer.executionContext)
+              newEntity
+            }
 
           val isEarlyResponse = messageEndPending && openRequests.isEmpty
           if (isEarlyResponse && response.status.isSuccess)