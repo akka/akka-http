a87761d6dd47b6817236fcc4481e4379b8568e0a
Arnout Engelen <arnout@bzzt.net>
2019-09-03 11:15:09 +0200

    Make the settings a bit more readable
    Though it remains subtle ;)

--- project/Common.scala
+++ project/Common.scala
@@ -22,31 +22,26 @@ object Common extends AutoPlugin {
     ),
     // '-release' parameter is restricted to 'Compile, compile' scope because
     // otherwise `sbt akka-http-xml/compile:doc` fails with it on Scala 2.12.9
-    scalacOptions in (Compile, compile) ++= {
-      if (scalaMinorVersion.value >= 12 && !sys.props("java.version").startsWith("1.") /* i.e. Java version >= 9 */)
-        Seq("-release", "8")
-      else
-        Nil
-    },
-    javacOptions ++= Seq(
-      "-encoding", "UTF-8",
-    ),
-    javacOptions ++= {
-      if (!sys.props("java.version").startsWith("1.") /* i.e. Java version >= 9 */)
-        Seq("--release", "8")
-      else
-        Seq("-source", "1.8")
-    },
+    scalacOptions in (Compile, compile) ++=
+      onlyAfterScala212(onlyAfterJdk8("-release", "8")).value,
+    javacOptions ++=
+      Seq("-encoding", "UTF-8") ++ onlyOnJdk8("-source", "1.8") ++ onlyAfterJdk8("--release", "8"),
     // restrict to 'compile' scope because otherwise it is also passed to
     // javadoc and -target is not valid there.
     // https://github.com/sbt/sbt/issues/1785
-    javacOptions in (Compile, compile) ++= {
-      if (!sys.props("java.version").startsWith("1.") /* i.e. Java version >= 9 */)
-        Nil
-      else
-        Seq("-target", "1.8")
-    },
+    javacOptions in (Compile, compile) ++=
+      // From jdk9 onwards this is covered by the '-release' flag above
+      onlyOnJdk8("-target", "1.8"),
   )
+
+  val specificationVersion: String = sys.props("java.specification.version")
+  def isJdk8: Boolean =
+    VersionNumber(specificationVersion).matchesSemVer(SemanticSelector(s"=1.8"))
+  def onlyOnJdk8[T](values: T*): Seq[T] = if (isJdk8) values else Seq.empty[T]
+  def onlyAfterJdk8[T](values: T*): Seq[T] = if (isJdk8) Seq.empty[T] else values
+  def onlyAfterScala212[T](values: Seq[T]): Def.Initialize[Seq[T]] = Def.setting {
+    if (scalaMinorVersion.value >= 12) values else Seq.empty[T]
+  }
 
   def scalaMinorVersion: Def.Initialize[Long] = Def.setting { CrossVersion.partialVersion(scalaVersion.value).get._2 }
 
