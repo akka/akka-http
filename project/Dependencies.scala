988f1046e78129b4f0b8e5955a6f8f83aa03dfde
Scala Steward <me@scala-steward.org>
2019-10-05 00:09:16 +0200

    Update gson to 2.8.6

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -60,7 +60,7 @@ object Dependencies {
 
     object Docs {
       val sprayJson   = Compile.sprayJson                                                                    % "test"
-      val gson        = "com.google.code.gson"             % "gson"                    % "2.8.5"             % "test"
+      val gson        = "com.google.code.gson"             % "gson"                    % "2.8.6"             % "test"
       val jacksonXml  = "com.fasterxml.jackson.dataformat" % "jackson-dataformat-xml"  % jacksonXmlVersion   % "test" // ApacheV2
       val reflections = "org.reflections"                  % "reflections"             % "0.9.11"            % "test" // WTFPL
     }
