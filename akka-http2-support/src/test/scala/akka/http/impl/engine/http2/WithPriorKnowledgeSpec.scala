261a70a785f809383a38b594117d99ce945fd70f
Arnout Engelen <arnout@bzzt.net>
2019-10-07 08:47:07 +0000

    Allow sending an ack SETTINGS frame immediately (#2735)
    Might fix #2734
    
    * More diagnostics
    * Add SilenceAllTestEventListener, close source

--- akka-http2-support/src/test/scala/akka/http/impl/engine/http2/WithPriorKnowledgeSpec.scala
+++ akka-http2-support/src/test/scala/akka/http/impl/engine/http2/WithPriorKnowledgeSpec.scala
@@ -6,6 +6,7 @@ package akka.http.impl.engine.http2
 
 import java.util.Base64
 
+import akka.http.impl.util.WithLogCapturing
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model.{ HttpProtocols, HttpRequest, HttpResponse, StatusCodes }
 import akka.stream.{ ActorMaterializer, OverflowStrategy }
@@ -16,10 +17,11 @@ import akka.util.ByteString
 import scala.concurrent.Future
 
 class WithPriorKnowledgeSpec extends AkkaSpec("""
-    akka.loglevel = warning
-    akka.loggers = ["akka.testkit.TestEventListener"]
+    akka.loglevel = debug
+    akka.loggers = ["akka.http.impl.util.SilenceAllTestEventListener"]
     akka.http.server.preview.enable-http2 = on
-  """) {
+    akka.http.server.http2.log-frames = on
+  """) with WithLogCapturing {
 
   implicit val ec = system.dispatcher
 
@@ -55,9 +57,17 @@ class WithPriorKnowledgeSpec extends AkkaSpec("""
       source.offer("UFJJICogSFRUUC8yLjANCg0KU00NCg0KAAASBAAAAAAAAAMAAABkAARAAAAAAAIAAAAAAAAECAAAAAAAP/8AAQAAHgEFAAAAAYKEhkGKCJ1cC4Fw3HwAf3qIJbZQw6u20uBTAyovKg==").futureValue
 
       // read settings frame
-      Http2Protocol.FrameType.byId(sink.pull().futureValue.get(3)) should be(Http2Protocol.FrameType.SETTINGS)
-      // read settings frame
-      Http2Protocol.FrameType.byId(sink.pull().futureValue.get(3)) should be(Http2Protocol.FrameType.SETTINGS)
+      val responseSettings = sink.pull().futureValue
+      Http2Protocol.FrameType.byId(responseSettings.get(3)) should be(Http2Protocol.FrameType.SETTINGS)
+      val isAck = responseSettings.get(4) == 1
+
+      if (!isAck) {
+        // read settings 'ack' frame
+        val ackResponseSettings = sink.pull().futureValue
+        Http2Protocol.FrameType.byId(ackResponseSettings.get(3)) should be(Http2Protocol.FrameType.SETTINGS)
+        ackResponseSettings.get(4) should be(1)
+      }
+
       // ack settings
       source.offer("AAAABAEAAAAA")
 
@@ -65,6 +75,8 @@ class WithPriorKnowledgeSpec extends AkkaSpec("""
       val tpe = Http2Protocol.FrameType.byId(response(3))
       tpe should be(Http2Protocol.FrameType.HEADERS)
       response.map(_.toChar).mkString should include("418")
+
+      source.complete()
     }
   }
 
