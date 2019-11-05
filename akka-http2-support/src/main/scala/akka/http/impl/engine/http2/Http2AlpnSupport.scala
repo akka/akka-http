5ef32b8b1daeb0b39f9ba1fbde49a21a9b13e614
João Ferreira <jtjeferreira@gmail.com>
2019-11-05 23:12:04 +0000

    fix feedback

--- akka-http2-support/src/main/scala/akka/http/impl/engine/http2/Http2AlpnSupport.scala
+++ akka-http2-support/src/main/scala/akka/http/impl/engine/http2/Http2AlpnSupport.scala
@@ -4,7 +4,6 @@
 
 package akka.http.impl.engine.http2
 
-import java.util.Optional
 import java.util.function.BiFunction
 import java.{ util => ju }
 
@@ -46,9 +45,12 @@ private[http] object Http2AlpnSupport {
     engine.asInstanceOf[JDK9SSLEngine].setHandshakeApplicationProtocolSelector(new BiFunction[SSLEngine, ju.List[String], String] {
       // explicit style needed here as automatic SAM-support doesn't seem to work out with Scala 2.11
       override def apply(engine: SSLEngine, protocols: ju.List[String]): String = {
-        val chosen = chooseProtocol(protocols).orElse(null)
-        setChosenProtocol(chosen)
-        chosen
+        val chosen = chooseProtocol(protocols)
+        chosen.foreach(setChosenProtocol)
+
+        //returning null here means aborting the handshake
+        //see https://docs.oracle.com/en/java/javase/11/docs/api/java.base/javax/net/ssl/SSLEngine.html#setHandshakeApplicationProtocolSelector(java.util.function.BiFunction)
+        chosen.orNull
       }
     })
 
@@ -59,7 +61,9 @@ private[http] object Http2AlpnSupport {
     ALPN.put(engine, new ServerProvider {
       override def select(protocols: ju.List[String]): String =
         choose {
-          chooseProtocol(protocols).orElseThrow(() => new SSLException("No protocols"))
+          //throwing an exception means aborting the handshake
+          //see http://git.eclipse.org/c/jetty/org.eclipse.jetty.alpn.git/tree/src/main/java/org/eclipse/jetty/alpn/ALPN.java#n236
+          chooseProtocol(protocols).getOrElse(throw new SSLException("No protocols"))
         }
 
       override def unsupported(): Unit =
@@ -73,8 +77,10 @@ private[http] object Http2AlpnSupport {
     engine
   }
 
-  private def chooseProtocol(protocols: ju.List[String]): Optional[String] =
-    protocols.stream().filter(p => p == H2 || p == HTTP11).findFirst()
+  private def chooseProtocol(protocols: ju.List[String]): Option[String] =
+    if (protocols.contains(H2)) Some(H2)
+    else if (protocols.contains(HTTP11)) Some(HTTP11)
+    else None
 
   def applySessionParameters(engine: SSLEngine, sessionParameters: NegotiateNewSession): Unit = TlsUtils.applySessionParameters(engine, sessionParameters)
 
