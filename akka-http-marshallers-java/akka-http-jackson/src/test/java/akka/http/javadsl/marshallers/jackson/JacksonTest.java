56f319b36080835bd2114b018d859f2702a97604
Akka Jenkins Autoformatter <johannes.rudolph@gmail.com>
2020-01-20 13:27:15 +0000

    Autoformatted code

--- akka-http-marshallers-java/akka-http-jackson/src/test/java/akka/http/javadsl/marshallers/jackson/JacksonTest.java
+++ akka-http-marshallers-java/akka-http-jackson/src/test/java/akka/http/javadsl/marshallers/jackson/JacksonTest.java
@@ -1,6 +1,7 @@
 /*
- * Copyright (C) 2009-2020 Lightbend Inc. <http://www.lightbend.com>
+ * Copyright (C) 2009-2019 Lightbend Inc. <https://www.lightbend.com>
  */
+
 package akka.http.javadsl.marshallers.jackson;
 
 import akka.actor.ActorSystem;
