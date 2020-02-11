2ec155ad993ca7d6b573d64e43f69fc9efe49557
Johannes Rudolph <johannes.rudolph@gmail.com>
2020-02-11 11:04:43 +0100

    core: prevent initialization NPE which might fail all retries quickly
    Fixes #2951

--- akka-http-core/src/test/scala/akka/http/impl/engine/client/ConnectionPoolSpec.scala
+++ akka-http-core/src/test/scala/akka/http/impl/engine/client/ConnectionPoolSpec.scala
@@ -264,7 +264,8 @@ abstract class ConnectionPoolSpec(poolImplementation: PoolImplementation)
       requestIn.sendNext(HttpRequest(uri = "/crash") -> 43)
       responseOutSub.request(2)
 
-      val remainingResponsesToKill = new AtomicInteger(1)
+      // must be lazy to prevent initialization problem because `mapServerSideOutboundRawBytes` might be called before the value is initialized
+      lazy val remainingResponsesToKill = new AtomicInteger(1)
       override def mapServerSideOutboundRawBytes(bytes: ByteString): ByteString =
         if (bytes.utf8String.contains("/crash") && remainingResponsesToKill.decrementAndGet() >= 0)
           sys.error("CRASH BOOM BANG")
@@ -281,14 +282,14 @@ abstract class ConnectionPoolSpec(poolImplementation: PoolImplementation)
       if (poolImplementation == PoolImplementation.Legacy)
         pending
 
-      val remainingResponsesToKill = new AtomicInteger(5)
-
       val (requestIn, responseOut, responseOutSub, _) = cachedHostConnectionPool[Int](maxRetries = 4)
 
       requestIn.sendNext(HttpRequest(uri = "/a") -> 42)
       requestIn.sendNext(HttpRequest(uri = "/crash") -> 43)
       responseOutSub.request(2)
 
+      // must be lazy to prevent initialization problem because `mapServerSideOutboundRawBytes` might be called before the value is initialized
+      lazy val remainingResponsesToKill = new AtomicInteger(5)
       override def mapServerSideOutboundRawBytes(bytes: ByteString): ByteString =
         if (bytes.utf8String.contains("/crash") && remainingResponsesToKill.decrementAndGet() >= 0)
           sys.error("CRASH BOOM BANG")
@@ -311,7 +312,7 @@ abstract class ConnectionPoolSpec(poolImplementation: PoolImplementation)
       val (requestIn, responseOut, responseOutSub, hcp) = cachedHostConnectionPool[Int](idleTimeout = 200.millis)
       val gateway = hcp.gateway
 
-      val responseEntityPub = TestPublisher.probe[ByteString]()
+      lazy val responseEntityPub = TestPublisher.probe[ByteString]()
 
       override def testServerHandler(connNr: Int): HttpRequest => HttpResponse = {
         case request @ HttpRequest(_, Uri.Path("/a"), _, _, _) =>
@@ -762,7 +763,7 @@ abstract class ConnectionPoolSpec(poolImplementation: PoolImplementation)
 
   implicit class MustContain[T](specimen: Seq[T]) {
     def mustContainLike(pf: PartialFunction[T, Unit]): Unit =
-      specimen.collectFirst(pf) getOrElse fail("did not contain")
+      specimen.collectFirst(pf) getOrElse fail(s"None of [${specimen.mkString(", ")}] matched partial function")
   }
 
   object NoErrorComplete extends SingletonException
