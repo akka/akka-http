3a2db8c8e8d91d735b5ae2bcacf8cd1df885c09e
Johannes Rudolph <johannes.rudolph@gmail.com>
2020-03-31 16:24:51 +0200

    core: prepend headers in `HttpMessage.addHeader` (#3029)
    * core: prepend headers in `HttpMessage.addHeader`
    
    Since by default headers are stored in a list, we would slowly append to the
    list. It's questionable whether List is the right datastructure here but
    that can be changed separately if needed.
    
    Fixes #3028
    
    * core: avoid `mapHeaders` inside of HttpMessage implementations
    
    Since `mapHeaders` is a higher-order function it has a higher chance of
    preventing inlining. Inside of HttpMessage implementations, mapHeaders is
    not even simpler than using `withHeaders` directly.

--- akka-http-core/src/main/scala/akka/http/scaladsl/model/HttpMessage.scala
+++ akka-http-core/src/main/scala/akka/http/scaladsl/model/HttpMessage.scala
@@ -158,7 +158,8 @@ sealed trait HttpMessage extends jm.HttpMessage {
    */
   def connectionCloseExpected: Boolean = HttpMessage.connectionCloseExpected(protocol, header[Connection])
 
-  def addHeader(header: jm.HttpHeader): Self = mapHeaders(_ :+ header.asInstanceOf[HttpHeader])
+  /** Return a new instance with the given header added to the headers sequence. It's undefined where the header is added to the sequence */
+  def addHeader(header: jm.HttpHeader): Self = withHeaders(header.asInstanceOf[HttpHeader] +: headers)
 
   def addAttribute[T](key: jm.AttributeKey[T], value: T): Self = {
     val ev = implicitly[JavaMapping[jm.AttributeKey[T], AttributeKey[T]]]
@@ -170,7 +171,7 @@ sealed trait HttpMessage extends jm.HttpMessage {
   /** Removes the header with the given name (case-insensitive) */
   def removeHeader(headerName: String): Self = {
     val lowerHeaderName = headerName.toRootLowerCase
-    mapHeaders(_.filterNot(_.is(lowerHeaderName)))
+    withHeaders(headers.filterNot(_.is(lowerHeaderName)))
   }
 
   def removeAttribute(key: jm.AttributeKey[_]): Self = {
@@ -210,7 +211,7 @@ sealed trait HttpMessage extends jm.HttpMessage {
     Util.convertOption(headers.find(_.is(lowerCased))) // Upcast because of invariance
   }
   /** Java API */
-  def addHeaders(headers: JIterable[jm.HttpHeader]): Self = mapHeaders(_ ++ headers.asScala.asInstanceOf[Iterable[HttpHeader]])
+  def addHeaders(headers: JIterable[jm.HttpHeader]): Self = withHeaders(this.headers ++ headers.asScala.asInstanceOf[Iterable[HttpHeader]])
   /** Java API */
   def withHeaders(headers: JIterable[jm.HttpHeader]): Self = {
     import JavaMapping.Implicits._
