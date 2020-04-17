2bc6f00b3656f2279c62f0c1f78637134c4c443f
Arnout Engelen <arnout@bzzt.net>
2020-04-17 13:19:16 +0200

    Actor interop Java example #2664

---
+++ docs/src/test/java/docs/http/javadsl/JobRoutes.java
@@ -0,0 +1,82 @@
+package docs.http.javadsl;
+
+//#akka-typed-json
+import akka.http.javadsl.marshallers.jackson.Jackson;
+
+//#akka-typed-json
+
+//#akka-typed-route
+import java.time.Duration;
+import java.util.concurrent.CompletionStage;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.ActorSystem;
+import akka.actor.typed.javadsl.AskPattern;
+
+import akka.http.javadsl.model.StatusCodes;
+import akka.http.javadsl.server.Route;
+
+import static akka.http.javadsl.server.Directives.*;
+import static akka.http.javadsl.unmarshalling.StringUnmarshallers.LONG;
+
+/** Routes for use with the HttpServerWithActorsSample */
+public class JobRoutes {
+    private static Route addOrDelete(ActorRef<JobRepository.Command> buildJobRepository, ActorSystem<?> system) {
+        return concat(
+                post(() ->
+                    entity(Jackson.unmarshaller(JobRepository.Job.class), job -> {
+                        CompletionStage<JobRepository.Response> operationPerformed =  AskPattern.ask(
+                                buildJobRepository,
+                                replyTo -> new JobRepository.AddJob(job, replyTo),
+                                Duration.ofSeconds(3),
+                                system.scheduler());
+                        return onSuccess(operationPerformed, response -> {
+                            if (response instanceof JobRepository.OK) {
+                                return complete("Job added");
+                            } else if (response instanceof JobRepository.KO) {
+                                return complete(StatusCodes.INTERNAL_SERVER_ERROR, ((JobRepository.KO)response).reason);
+                            } else {
+                                return complete(StatusCodes.INTERNAL_SERVER_ERROR);
+                            }
+                        });
+                    })),
+                delete(() -> {
+                    CompletionStage<JobRepository.Response> operationPerformed = AskPattern.ask(
+                            buildJobRepository,
+                            JobRepository.ClearJobs::new,
+                            Duration.ofSeconds(3),
+                            system.scheduler());
+                    return onSuccess(operationPerformed, response -> {
+                            if (response instanceof JobRepository.OK) {
+                                return complete("Jobs cleared");
+                            } else if (response instanceof JobRepository.KO) {
+                                return complete(StatusCodes.INTERNAL_SERVER_ERROR, ((JobRepository.KO)response).reason);
+                            } else {
+                                return complete(StatusCodes.INTERNAL_SERVER_ERROR);
+                            }
+                    });
+                })
+        );
+    }
+
+    public static Route jobRoutes(ActorRef<JobRepository.Command> buildJobRepository, ActorSystem<?> system) {
+        return pathPrefix("jobs", () ->
+                concat(
+                        pathEnd(() ->
+                                addOrDelete(buildJobRepository, system)
+                        ),
+                        get(() ->
+                                path(LONG, jobId -> {
+                                    CompletionStage<JobRepository.Job> maybeJob = AskPattern.ask(
+                                            buildJobRepository,
+                                            replyTo -> new JobRepository.GetJobById(jobId, replyTo),
+                                            Duration.ofSeconds(3),
+                                            system.scheduler());
+                                    return onSuccess(maybeJob, job ->
+                                            complete(StatusCodes.OK, job, Jackson.<JobRepository.Job>marshaller()));
+                                })
+                        )
+                )
+        );
+    }
+}
