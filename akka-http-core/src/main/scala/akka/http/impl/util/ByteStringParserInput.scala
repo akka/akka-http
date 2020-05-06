d3b1322d4526171370935ec7b4648bc45fabc672
Johannes Rudolph <johannes.rudolph@gmail.com>
2020-05-06 12:06:55 +0200

    Reuse UriParser In HttpRequestParser (#3108)
    * parsing: allow reusing of parser including ValueStack
    
    This is not quite done API-wise. You can now reuse a parser by providing
    different values for `input`. Allocating the ValueStack turned up a lot
    in profiling.
    
    * core: precalculate size for ByteStringParserInput
    
    * core: reuse UriParser in HttpRequestParser
    
    Together with the other changes, this seems to be a big performance
    improvement. Mostly, because the ValueStack is now only allocated once
    and then reused across requests.

--- akka-http-core/src/main/scala/akka/http/impl/util/ByteStringParserInput.scala
+++ akka-http-core/src/main/scala/akka/http/impl/util/ByteStringParserInput.scala
@@ -24,8 +24,8 @@ import akka.util.ByteString
  */
 @InternalApi
 final private[http] class ByteStringParserInput(bytes: ByteString) extends DefaultParserInput {
+  val length: Int = bytes.size
   override def charAt(ix: Int): Char = (bytes(ix) & 0xFF).toChar
-  override def length: Int = bytes.size
   override def sliceString(start: Int, end: Int): String = bytes.slice(start, end).decodeString(StandardCharsets.ISO_8859_1)
   override def sliceCharArray(start: Int, end: Int): Array[Char] =
     StandardCharsets.ISO_8859_1.decode(bytes.slice(start, end).asByteBuffer).array()
