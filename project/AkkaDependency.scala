dc1c71afd90465908a32079b717529035da2ee14
Arnout Engelen <arnout@bzzt.net>
2020-06-04 11:30:55 +0200

    Update Akka to fix TLS1.3 on JDK11

--- project/AkkaDependency.scala
+++ project/AkkaDependency.scala
@@ -42,9 +42,10 @@ object AkkaDependency {
   }
 
   // Default version updated only when needed, https://doc.akka.io//docs/akka/current/project/downstream-upgrade-strategy.html
-  val minimumExpectedAkkaVersion = "2.5.31"
+  // Needs https://github.com/akka/akka/pull/29148
+  val minimumExpectedAkkaVersion = "2.5-20200529-210210"
   val default = akkaDependency(defaultVersion = minimumExpectedAkkaVersion)
-  val minimumExpectedAkka26Version = "2.6.4"
+  val minimumExpectedAkka26Version = "2.6.5+106-33d64f9d"
   val docs = akkaDependency(defaultVersion = minimumExpectedAkka26Version)
 
   val akkaVersion: String = default match {
@@ -78,7 +79,8 @@ object AkkaDependency {
                 else
                   "com.typesafe.akka" %% module % akkaVersion % config
               },
-              resolvers ++= (if (akkaSnapshot) Seq("Akka Snapshots" at "https://repo.akka.io/snapshots") else Nil)
+              //resolvers ++= (if (akkaSnapshot) Seq("Akka Snapshots" at "https://repo.akka.io/snapshots") else Nil)
+              resolvers += "Akka Snapshots" at "https://repo.akka.io/snapshots"
             )
         }
       }
