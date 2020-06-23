a17051e634fb0fd156913fd7b0f3a2410c1bb002
Johannes Rudolph <johannes.rudolph@gmail.com>
2020-06-23 14:00:57 +0200

    core: refactor access to pools and rename PoolGateway -> PoolInterface

---
+++ akka-http-compatibility-tests/src/main/scala/example/HostConnectionPoolCompat.scala
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2020 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package example
+
+import akka.http.scaladsl.Http.HostConnectionPool
+
+object HostConnectionPoolCompat {
+  def access(hcp: HostConnectionPool): Unit = {
+    val theSetup = hcp.setup
+    hcp.shutdown()
+
+    hcp match {
+      // This still works because case class matching does not require an unapply method
+      case HostConnectionPool(setup) => require(setup == theSetup)
+    }
+
+    require(hcp.productArity == 1)
+    require(hcp.productElement(0) == hcp.setup)
+    require(hcp.canEqual(hcp))
+    require(hcp.equals(hcp))
+
+    // Companion object is still there, even if had no good public use
+    HostConnectionPool
+
+    // This one didn't compile even before (private[http] constructor)
+    // new HostConnectionPool(hcp.setup)(null)
+
+    // This line compiles but only by giving null as the PoolGateway parameter which was private before
+    // This would crash now
+    // HostConnectionPool(hcp.setup)(null)
+
+    // These lines compile but require nulling out the gateway parameter
+    // They would crash now.
+    // hcp.copy(setup = hcp.setup)(null)
+    // hcp.copy()(null)
+
+    // This compiles but crashes now but is unlikely user code
+    // val Some(setup) = HostConnectionPool.unapply(hcp)
+  }
+}
---
+++ akka-http-core/src/main/scala/akka/http/impl/engine/client/PoolId.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2009-2020 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.http.impl.engine.client
+
+import java.util.concurrent.atomic.AtomicLong
+
+import akka.http.impl.engine.client.PoolId.PoolUsage
+import akka.http.impl.settings.HostConnectionPoolSetup
+
+/**
+ * Represents a pool by its target endpoint and settings and its designated usage (shared or unique).
+ *
+ * A [[PoolId]] is represented by its [[HostConnectionPoolSetup]] and its [[PoolUsage]]. If the later
+ * is [[SharedPool]], it means that a shared pool must be used for this particular [[HostConnectionPoolSetup]].
+ */
+private[http] final class PoolId(val hcps: HostConnectionPoolSetup, val usage: PoolUsage) {
+  override def toString = s"PoolId(hcps = $hcps, usage = $usage)"
+
+  override def equals(that: Any): Boolean =
+    that match {
+      case p: PoolId => p.hcps == hcps && p.usage == usage
+      case _         => false
+    }
+
+  override def hashCode(): Int = hcps.hashCode() ^ usage.hashCode()
+}
+
+private[http] object PoolId {
+
+  sealed trait PoolUsage {
+    def name: String
+  }
+  case object SharedPool extends PoolUsage {
+    def name: String = "shared"
+  }
+  final case class UniquePool(id: Long) extends PoolUsage {
+    def name: String = s"#$id"
+  }
+
+  private[this] val uniquePoolId = new AtomicLong(0)
+  def newUniquePool() = UniquePool(uniquePoolId.incrementAndGet())
+}
