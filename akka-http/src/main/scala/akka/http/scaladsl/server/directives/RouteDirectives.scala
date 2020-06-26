5b2d816c0801de49a14048f2ddde88d940e7e95b
Arnout Engelen <arnout@bzzt.net>
2020-06-26 13:42:32 +0200

    Allow statuscode/headers/entity as well

--- akka-http/src/main/scala/akka/http/scaladsl/server/directives/RouteDirectives.scala
+++ akka-http/src/main/scala/akka/http/scaladsl/server/directives/RouteDirectives.scala
@@ -6,6 +6,7 @@ package akka.http.scaladsl.server
 package directives
 
 import scala.concurrent.Future
+import scala.collection.immutable
 import akka.http.scaladsl.marshalling.{ ToEntityMarshaller, ToResponseMarshallable }
 import akka.http.scaladsl.model._
 import StatusCodes._
@@ -55,6 +56,14 @@ trait RouteDirectives {
    */
   def complete[T](status: StatusCode, v: T)(implicit m: ToEntityMarshaller[T]): StandardRoute =
     complete((status, v))
+
+  /**
+   * Completes the request using the given arguments.
+   *
+   * @group route
+   */
+  def complete[T](status: StatusCode, headers: immutable.Seq[HttpHeader], v: T)(implicit m: ToEntityMarshaller[T]): StandardRoute =
+    complete((status, headers, v))
 
   /**
    * Bubbles the given error up the response chain, where it is dealt with by the closest `handleExceptions`
