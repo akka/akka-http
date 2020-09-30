cd9a4ab12375d958eebb8b12fe1834adf2a73e3a
Arnout Engelen <arnout@bzzt.net>
2020-09-30 15:26:46 +0200

    http: provide implicit conversion from route and materializer to flow (#3489)
    Fixes #3427

---
+++ akka-http-tests/src/test/scala/akka/http/scaladsl/server/RouteResultSpec.scala
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2020 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.http.scaladsl.server
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.model.{ HttpRequest, HttpResponse, StatusCodes }
+import akka.http.scaladsl.server.Directives.complete
+import akka.stream.{ Materializer, SystemMaterializer }
+import akka.stream.scaladsl.Flow
+import akka.testkit.TestKit
+import com.github.ghik.silencer.silent
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+@silent("never used")
+class RouteResultSpec extends AnyWordSpec with Matchers {
+  "RouteResult" should {
+    val route: Route = complete(StatusCodes.OK)
+    "provide a conversion from Route to Flow when an ActorSystem is available" in {
+      implicit val system = ActorSystem("RouteResultSpec1")
+
+      val flow: Flow[HttpRequest, HttpResponse, Any] = route
+
+      TestKit.shutdownActorSystem(system, verifySystemShutdown = true)
+    }
+
+    "provide a conversion from Route to Flow when a Materializer is implicitly available" in {
+      val system = ActorSystem("RouteResultSpec1")
+      implicit val materializer: Materializer = SystemMaterializer(system).materializer
+
+      @silent("deprecated")
+      val flow: Flow[HttpRequest, HttpResponse, Any] = route
+
+      TestKit.shutdownActorSystem(system, verifySystemShutdown = true)
+    }
+
+    "provide a conversion from Route to Flow when both a Materializer and a system are implicitly  available" in {
+      implicit val system = ActorSystem("RouteResultSpec1")
+      // implemented with ??? so it produces an error when the materializer is selected over the system
+      implicit def materializer: Materializer = ???
+
+      val flow: Flow[HttpRequest, HttpResponse, Any] = route
+
+      TestKit.shutdownActorSystem(system, verifySystemShutdown = true)
+    }
+  }
+
+}
