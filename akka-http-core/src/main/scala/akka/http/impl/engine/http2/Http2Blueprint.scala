237b45a21f97b1e4901edab8727d9861e98d7eb3
Arnout Engelen <arnout@bzzt.net>
2020-11-03 12:27:33 +0100

    WiP (impressive scala error on Http2ServerDemux:37)

--- akka-http-core/src/main/scala/akka/http/impl/engine/http2/Http2Blueprint.scala
+++ akka-http-core/src/main/scala/akka/http/impl/engine/http2/Http2Blueprint.scala
@@ -15,7 +15,9 @@ import akka.http.impl.engine.http2.hpack.{ HeaderCompression, HeaderDecompressio
 import akka.http.impl.engine.parsing.HttpHeaderParser
 import akka.http.impl.util.LogByteStringTools.logTLSBidiBySetting
 import akka.http.impl.util.StreamUtils
+import akka.http.scaladsl.model.HttpEntity.LastChunk
 import akka.http.scaladsl.model._
+import akka.http.scaladsl.model.headers.RawHeader
 import akka.http.scaladsl.settings.{ ClientConnectionSettings, Http2CommonSettings, ParserSettings, ServerSettings }
 import akka.stream.TLSProtocol._
 import akka.stream.scaladsl.{ BidiFlow, Flow, Source }
@@ -59,13 +61,16 @@ private[http2] final case class ByteHttp2SubStream(
 private[http2] final case class ChunkedHttp2SubStream(
   initialHeaders:        ParsedHeadersFrame,
   data:                  Source[HttpEntity.ChunkStreamPart, Any],
+  trailingHeaders:       Future[ParsedHeadersFrame],
   correlationAttributes: Map[AttributeKey[_], _]                 = Map.empty
 ) extends Http2SubStream {
   override def withCorrelationAttributes(attributes: Map[AttributeKey[_], _]): Http2SubStream =
     copy(correlationAttributes = attributes)
 
-  def createResponseEntity(contentType: ContentType): RequestEntity =
-    HttpEntity.Chunked(contentType, data)
+  def createResponseEntity(contentType: ContentType)(implicit ec: ExecutionContext): RequestEntity =
+    HttpEntity.Chunked(contentType, data.concat(Source.lazilyAsync(() => trailingHeaders.map(frame => LastChunk(extension = "", frame.keyValuePairs.map {
+      case (k, v) => RawHeader(k, v)
+    })))))
 }
 
 /** INTERNAL API */
