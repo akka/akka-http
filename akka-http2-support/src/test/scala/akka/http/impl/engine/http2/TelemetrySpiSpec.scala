91420cfa578fc5a5aeb8d1ab745069488a55889b
Ignasi Marimon-Clos <ignasi@lightbend.com>
2020-12-14 12:24:53 +0100

    Run also in plaintext. Refactor: extract common code.

--- akka-http2-support/src/test/scala/akka/http/impl/engine/http2/TelemetrySpiSpec.scala
+++ akka-http2-support/src/test/scala/akka/http/impl/engine/http2/TelemetrySpiSpec.scala
@@ -45,7 +45,10 @@ class TestTelemetryImpl(system: ActorSystem) extends TelemetrySpi {
 
 }
 
-class TelemetrySpiSpec extends AkkaSpecWithMaterializer(
+class TelemetrySpiPlaintextSpec extends TelemetrySpiSpec(false)
+class TelemetrySpiCypherSpec extends TelemetrySpiSpec(true)
+
+abstract class TelemetrySpiSpec(useTls: Boolean) extends AkkaSpecWithMaterializer(
   """
      akka.http.server.preview.enable-http2 = on
      akka.actor.serialize-messages = false
@@ -57,6 +60,37 @@ class TelemetrySpiSpec extends AkkaSpecWithMaterializer(
 
   case class ConnectionId(is: String) extends Attribute
 
+  def foo(probe: TestProbe): (Http.ServerBinding, Flow[HttpRequest, HttpResponse, Future[Http.OutgoingConnection]]) = {
+    val handler: HttpRequest => Future[HttpResponse] = { req =>
+      req.headers.find(_.lowercaseName == "req-id").foreach(found => probe.ref ! found.value)
+      Future.successful(HttpResponse())
+    }
+
+    val serverBinding =
+      if (useTls) {
+        Http().newServerAt("localhost", 0)
+          .enableHttps(ExampleHttpContexts.exampleServerContext)
+          .bind(handler).futureValue
+      } else {
+        Http().newServerAt("localhost", 0)
+          .bind(handler).futureValue
+      }
+
+    val http2ClientFlow =
+      if (useTls) {
+        Http().connectionTo("akka.example.org")
+          .withCustomHttpsConnectionContext(ExampleHttpContexts.exampleClientContext)
+          .withClientConnectionSettings(ClientConnectionSettings(system).withTransport(ExampleHttpContexts.proxyTransport(serverBinding.localAddress)))
+          .http2()
+      } else {
+        Http().connectionTo("akka.example.org")
+          .withClientConnectionSettings(ClientConnectionSettings(system).withTransport(ExampleHttpContexts.proxyTransport(serverBinding.localAddress)))
+          .http2WithPriorKnowledge()
+      }
+
+    (serverBinding, http2ClientFlow)
+  }
+
   "Support for telemetry" should {
 
     "hook into HTTP2 client requests" in {
@@ -66,7 +100,7 @@ class TelemetrySpiSpec extends AkkaSpecWithMaterializer(
           BidiFlow.fromFlows(
             StreamUtils.statefulAttrsMap[HttpRequest, HttpRequest] { attrs => req =>
               val reqId = RequestId(UUID.randomUUID().toString)
-              probe.ref ! "req-seen"
+              probe.ref ! "request-seen"
               attrs.get[TelemetryAttributes.ClientMeta].foreach(probe.ref ! _)
               probe.ref ! reqId
               req.addAttribute(requestIdAttr, reqId).addHeader(headers.RawHeader("req-id", reqId.id))
@@ -74,7 +108,7 @@ class TelemetrySpiSpec extends AkkaSpecWithMaterializer(
               done.foreach(_ => probe.ref ! "close-seen")(system.dispatcher)
             },
             Flow[HttpResponse].map { res =>
-              probe.ref ! "res-seen"
+              probe.ref ! "response-seen"
               probe.ref ! res.getAttribute(requestIdAttr).get
               res.removeAttribute(requestIdAttr)
             }
@@ -87,35 +121,23 @@ class TelemetrySpiSpec extends AkkaSpecWithMaterializer(
         override def serverConnection: BidiFlow[HttpResponse, HttpResponse, HttpRequest, HttpRequest, NotUsed] = BidiFlow.identity
       })
 
-      val handler: HttpRequest => Future[HttpResponse] = { req =>
-        req.headers.find(_.lowercaseName == "req-id").foreach(found => probe.ref ! found.value)
-        Future.successful(HttpResponse())
-      }
-      val binding =
-        Http().newServerAt("localhost", 0)
-          .enableHttps(ExampleHttpContexts.exampleServerContext)
-          .bind(handler).futureValue
-
-      val http2flow = Http().connectionTo("akka.example.org")
-        .withCustomHttpsConnectionContext(ExampleHttpContexts.exampleClientContext)
-        .withClientConnectionSettings(ClientConnectionSettings(system).withTransport(ExampleHttpContexts.proxyTransport(binding.localAddress)))
-        .http2()
+      val (serverBinding, http2ClientFow) = foo(probe)
 
       val (reqQueue, resQueue) =
         Source.queue(10, OverflowStrategy.fail)
-          .viaMat(http2flow)(Keep.left)
+          .viaMat(http2ClientFow)(Keep.left)
           .toMat(Sink.actorRef(probe.ref, "done"))(Keep.both)
           .run()
       reqQueue.offer(HttpRequest())
 
       probe.expectMsg("seen-connection")
-      probe.expectMsg("req-seen")
+      probe.expectMsg("request-seen")
       probe.expectMsgType[TelemetryAttributes.ClientMeta]
       val reqId = probe.expectMsgType[RequestId]
       val reqIdOnServer = probe.expectMsgType[String]
       reqIdOnServer should ===(reqId.id)
 
-      probe.expectMsg("res-seen")
+      probe.expectMsg("response-seen")
       val resId = probe.expectMsgType[RequestId]
       reqId should ===(resId)
       val res = probe.expectMsgType[HttpResponse]
@@ -123,14 +145,11 @@ class TelemetrySpiSpec extends AkkaSpecWithMaterializer(
       reqQueue.complete()
 
       probe.expectMsg("close-seen")
+      serverBinding.terminate(3.seconds).futureValue
     }
 
     "hook into HTTP2 server requests" in {
       val probe = TestProbe()
-      val handler: HttpRequest => Future[HttpResponse] = { req =>
-        req.headers.find(_.lowercaseName == "req-id").foreach(found => probe.ref ! found.value)
-        Future.successful(HttpResponse())
-      }
       TestTelemetryImpl.delegate = Some(new TelemetrySpi {
         override def client: BidiFlow[HttpRequest, HttpRequest, HttpResponse, HttpResponse, NotUsed] =
           BidiFlow.identity
@@ -147,34 +166,26 @@ class TelemetrySpiSpec extends AkkaSpecWithMaterializer(
 
         override def serverConnection: BidiFlow[HttpResponse, HttpResponse, HttpRequest, HttpRequest, NotUsed] = BidiFlow.fromFlows(
           Flow[HttpResponse].map { res =>
-            probe.ref ! "res-seen"
+            probe.ref ! "response-seen"
             res
           }.watchTermination() { (_, done) =>
             done.foreach(_ => probe.ref ! "close-seen")(system.dispatcher)
           },
           StreamUtils.statefulAttrsMap[HttpRequest, HttpRequest](attrs =>
             { req =>
-              probe.ref ! "req-seen"
+              probe.ref ! "request-seen"
               attrs.get[ConnectionId].foreach(probe.ref ! _)
               req
             }
           ))
       })
 
-      val binding =
-        Http().newServerAt("localhost", 0)
-          .enableHttps(ExampleHttpContexts.exampleServerContext)
-          .bind(handler).futureValue
-
-      val http2flow = Http().connectionTo("akka.example.org")
-        .withCustomHttpsConnectionContext(ExampleHttpContexts.exampleClientContext)
-        .withClientConnectionSettings(ClientConnectionSettings(system).withTransport(ExampleHttpContexts.proxyTransport(binding.localAddress)))
-        .http2()
+      val (serverBinding, http2ClientFlow) = foo(probe)
 
       val resProbe = TestProbe()
       val (reqQueue, _) =
         Source.queue(10, OverflowStrategy.fail)
-          .viaMat(http2flow)(Keep.left)
+          .viaMat(http2ClientFlow)(Keep.left)
           .toMat(Sink.actorRef(resProbe.ref, "done"))(Keep.both)
           .run()
       reqQueue.offer(HttpRequest())
@@ -182,10 +193,10 @@ class TelemetrySpiSpec extends AkkaSpecWithMaterializer(
       probe.expectMsg("connection-seen")
       val connId = probe.expectMsgType[ConnectionId]
 
-      probe.expectMsg("req-seen")
+      probe.expectMsg("request-seen")
       probe.expectMsgType[ConnectionId] should ===(connId)
 
-      probe.expectMsg("res-seen")
+      probe.expectMsg("response-seen")
       val res = resProbe.expectMsgType[HttpResponse]
       res.discardEntityBytes()
       reqQueue.complete()
@@ -193,7 +204,7 @@ class TelemetrySpiSpec extends AkkaSpecWithMaterializer(
       probe.expectMsg("close-seen")
       resProbe.expectMsg("done")
 
-      binding.terminate(3.seconds).futureValue
+      serverBinding.terminate(3.seconds).futureValue
       probe.expectMsg("unbind-seen")
     }
 
