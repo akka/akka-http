2ef90a04d48d690225d943ba1ee5e5d384bdcd16
nitikagarw <nitikagarw@gmail.com>
2021-02-03 00:05:23 +0530

    rc: log frame headers

--- akka-http-core/src/main/scala/akka/http/impl/engine/ws/FrameLogger.scala
+++ akka-http-core/src/main/scala/akka/http/impl/engine/ws/FrameLogger.scala
@@ -30,8 +30,9 @@ private[ws] object FrameLogger {
       .addAttributes(Attributes(LogLevels(Logging.DebugLevel, Logging.DebugLevel, Logging.DebugLevel)))
 
   def logEvent(frameEvent: FrameEvent): String = {
-    case class LogEntry(frameType: String, data: String)
+    case class LogEntry(frameType: String, length: Long, lastPart: String, data: String, flags: Option[String]*)
 
+    def flag(value: Boolean, name: String): Option[String] = if (value) Some(name) else None
     def hex(bytes: ByteString): String = {
       val num = math.min(maxBytes, bytes.size)
       val ellipsis = if (num < bytes.size) s" [... ${bytes.size - num} more bytes]" else ""
@@ -41,15 +42,19 @@ private[ws] object FrameLogger {
         .mkString(" ") + ellipsis
     }
 
-    def entryForFrame(frameEvent: FrameEvent): LogEntry =
+    def entryForFrame(frameEvent: FrameEvent): LogEntry = {
+      val lastPart = if (frameEvent.lastPart) "T" else "F"
       frameEvent match {
-        case FrameStart(_, data) => LogEntry("FRAMESTART", hex(data))
-        case FrameData(data, _)  => LogEntry("FRAMEDATA", hex(data))
+        case FrameStart(header, data) => LogEntry(header.opcode.toString.toUpperCase, header.length, lastPart, hex(data), flag(header.fin, "FIN"), flag(header.rsv1, "RSV1"), flag(header.rsv2, "RSV2"), flag(header.rsv3, "RSV3"))
+        case FrameData(data, _)       => LogEntry("FRAMEDATA", 0, lastPart, hex(data))
       }
+    }
 
     def display(entry: LogEntry): String = {
       import entry._
-      f"${Console.GREEN}$frameType%s ${Console.RESET}$data"
+      import Console._
+
+      f"$GREEN$frameType%s $YELLOW$length%4d $RED${flags.flatten.mkString(" ")} $BLUE$lastPart%s $RESET$data"
     }
 
     display(entryForFrame(frameEvent))
