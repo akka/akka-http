e3a4935151c91cee28e65e6b894dd50839ef9d34
Johan Andrén <johan@markatta.com>
2021-02-15 15:42:06 +0100

    core: disallow parsing of Transfer-Encoding other than chunked (#3754)

--- akka-http-core/src/test/scala/akka/http/impl/engine/parsing/RequestParserSpec.scala
+++ akka-http-core/src/test/scala/akka/http/impl/engine/parsing/RequestParserSpec.scala
@@ -155,17 +155,6 @@ abstract class RequestParserSpec(mode: String, newLine: String) extends AnyFreeS
         closeAfterResponseCompletion shouldEqual Seq(true)
       }
 
-      "with a funky `Transfer-Encoding` header" in new Test {
-        """PUT / HTTP/1.1
-          |Transfer-Encoding: foo, chunked, bar
-          |Host: x
-          |
-          |""" should parseTo(HttpRequest(PUT, "/", List(`Transfer-Encoding`(
-          TransferEncodings.Extension("foo"),
-          TransferEncodings.chunked, TransferEncodings.Extension("bar")), Host("x"))))
-        closeAfterResponseCompletion shouldEqual Seq(false)
-      }
-
       "with several identical `Content-Type` headers" in new Test {
         """GET /data HTTP/1.1
           |Host: x
@@ -339,17 +328,15 @@ abstract class RequestParserSpec(mode: String, newLine: String) extends AnyFreeS
       }
     }
 
-    "properly parse a chunked request with additional transfer encodings" in new Test {
+    "properly parse a chunked transfer encoding request" in new Test {
       """PATCH /data HTTP/1.1
-        |Transfer-Encoding: fancy, chunked
+        |Transfer-Encoding: chunked
         |Content-Type: application/pdf
         |Host: ping
         |
         |0
         |
-        |""" should parseTo(HttpRequest(PATCH, "/data", List(
-        `Transfer-Encoding`(TransferEncodings.Extension("fancy")),
-        Host("ping")), HttpEntity.Chunked(`application/pdf`, source(LastChunk))))
+        |""" should parseTo(HttpRequest(PATCH, "/data", List(Host("ping")), HttpEntity.Chunked(`application/pdf`, source(LastChunk))))
       closeAfterResponseCompletion shouldEqual Seq(false)
     }
 
@@ -666,6 +653,63 @@ abstract class RequestParserSpec(mode: String, newLine: String) extends AnyFreeS
           |Host: x
           |
           |""" should parseToError(422: StatusCode, ErrorInfo("TRACE requests must not have an entity"))
+      }
+
+      "a request with an unsupported transfer encoding" in new Test {
+        """PATCH /data HTTP/1.1
+          |Transfer-Encoding: fancy
+          |Content-Type: application/pdf
+          |Host: ping
+          |
+          |0
+          |
+          |""" should parseToError(BadRequest, ErrorInfo("Unsupported Transfer-Encoding 'fancy'"))
+      }
+
+      "a chunked request with additional transfer encodings" in new Test {
+        """PATCH /data HTTP/1.1
+          |Transfer-Encoding: fancy, chunked
+          |Content-Type: application/pdf
+          |Host: ping
+          |
+          |0
+          |
+          |""" should parseToError(BadRequest, ErrorInfo("Multiple Transfer-Encoding entries not supported"))
+      }
+
+      "a chunked request with additional transfer encodings after chunked in multiple headers" in new Test {
+        """PATCH /data HTTP/1.1
+          |Transfer-Encoding: chunked
+          |Transfer-Encoding: fancy
+          |Content-Type: application/pdf
+          |Host: ping
+          |
+          |0
+          |
+          |""" should parseToError(BadRequest, ErrorInfo("Multiple Transfer-Encoding entries not supported"))
+      }
+
+      "a chunked request with additional transfer encodings after chunked in one header" in new Test {
+        """PATCH /data HTTP/1.1
+          |Transfer-Encoding: chunked, fancy
+          |Content-Type: application/pdf
+          |Host: ping
+          |
+          |0
+          |
+          |""" should parseToError(BadRequest, ErrorInfo("Multiple Transfer-Encoding entries not supported"))
+      }
+
+      "a chunked request with a content length" in new Test {
+        """PATCH /data HTTP/1.1
+          |Transfer-Encoding: chunked
+          |Content-Type: application/pdf
+          |Content-Length: 7
+          |Host: ping
+          |
+          |0
+          |
+          |""" should parseToError(BadRequest, ErrorInfo("A chunked request must not contain a Content-Length header"))
       }
     }
   }
--- akka-http-core/src/test/scala/akka/http/impl/engine/parsing/ResponseParserSpec.scala
+++ akka-http-core/src/test/scala/akka/http/impl/engine/parsing/ResponseParserSpec.scala
@@ -110,20 +110,6 @@ abstract class ResponseParserSpec(mode: String, newLine: String) extends AnyFree
         closeAfterResponseCompletion shouldEqual Seq(false)
       }
 
-      "a response funky `Transfer-Encoding` header" in new Test {
-        override def parserSettings: ParserSettings =
-          super.parserSettings.withCustomStatusCodes(ServerOnTheMove)
-
-        """HTTP/1.1 331 Server on the move
-          |Transfer-Encoding: foo, chunked, bar
-          |Content-Length: 0
-          |
-          |""" should parseTo(HttpResponse(ServerOnTheMove, List(`Transfer-Encoding`(
-          TransferEncodings.Extension("foo"),
-          TransferEncodings.chunked, TransferEncodings.Extension("bar")))))
-        closeAfterResponseCompletion shouldEqual Seq(false)
-      }
-
       "a response with one header, a body, but no Content-Length header" in new Test {
         """HTTP/1.0 404 Not Found
           |Host: api.example.com
@@ -297,19 +283,6 @@ abstract class ResponseParserSpec(mode: String, newLine: String) extends AnyFree
         closeAfterResponseCompletion shouldEqual Seq(false)
       }
 
-      "response with additional transfer encodings" in new Test {
-        Seq("""HTTP/1.1 200 OK
-          |Transfer-Encoding: fancy, chunked
-          |Cont""", """ent-Type: application/pdf
-          |
-          |""") should generalMultiParseTo(
-          Right(HttpResponse(
-            headers = List(`Transfer-Encoding`(TransferEncodings.Extension("fancy"))),
-            entity = HttpEntity.Chunked(`application/pdf`, source()))),
-          Left(EntityStreamError(ErrorInfo("Entity stream truncation. The HTTP parser was receiving an entity when the underlying connection was closed unexpectedly."))))
-        closeAfterResponseCompletion shouldEqual Seq(false)
-      }
-
       "a response configured to override a built-in media type" in new Test {
         // Override the application/json media type and give it an open instead of fixed charset.
         // This allows us to support various third-party agents which use an explicit charset.
@@ -353,6 +326,29 @@ abstract class ResponseParserSpec(mode: String, newLine: String) extends AnyFree
           |Content-Length: 0
           |
           |""" should parseToError(MessageStartError(400: StatusCode, ErrorInfo("HTTP message must not contain more than one Content-Type header")))
+      }
+
+      "multiple transfer encodings in one header" in new Test {
+        """HTTP/1.1 200 OK
+          |Transfer-Encoding: fancy, chunked
+          |
+          |""" should parseToError(MessageStartError(BadRequest, ErrorInfo("Multiple Transfer-Encoding entries not supported")))
+      }
+
+      "multiple transfer encoding headers" in new Test {
+        """HTTP/1.1 200 OK
+          |Transfer-Encoding: chunked
+          |Transfer-Encoding: fancy
+          |
+          |""" should parseToError(MessageStartError(BadRequest, ErrorInfo("Multiple Transfer-Encoding entries not supported")))
+      }
+
+      "transfer encoding chunked and a content length" in new Test {
+        """HTTP/1.1 200 OK
+          |Transfer-Encoding: chunked
+          |Content-Length: 7
+          |
+          |""" should parseToError(MessageStartError(BadRequest, ErrorInfo("A chunked response must not contain a Content-Length header")))
       }
     }
   }
