ddee30da46f5b6c2fe854e8c644fe9f3925e56e5
Johannes Rudolph <johannes.rudolph@gmail.com>
2021-03-08 14:23:48 +0100

    build: update to Scala 2.13.5, 2.12.13 and scalafix 0.9.26
    Also update silencer to compatible version.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   val h2specName = s"h2spec_${DependencyHelpers.osName}_amd64"
   val h2specExe = "h2spec" + DependencyHelpers.exeIfWindows
   val h2specUrl = s"https://github.com/summerwind/h2spec/releases/download/v${h2specVersion}/${h2specName}.zip"
-  val silencerVersion = "1.7.1"
+  val silencerVersion = "1.7.3"
 
   val scalaTestVersion = "3.1.4"
   val specs2Version = "4.10.6"
@@ -26,8 +26,8 @@ object Dependencies {
 
   val scalafixVersion = _root_.scalafix.sbt.BuildInfo.scalafixVersion // grab from plugin
 
-  val scala212Version = "2.12.12"
-  val scala213Version = "2.13.3"
+  val scala212Version = "2.12.13"
+  val scala213Version = "2.13.5"
 
   val Versions = Seq(
     crossScalaVersions := Seq(scala213Version, scala212Version),
