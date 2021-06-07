62b101e47fb01c5ac175817f5a688ac1da8f976c
Johannes Rudolph <johannes.rudolph@gmail.com>
2021-06-07 12:52:14 +0200

    http2: DRY up different benchmarks to support same parameters

--- akka-http-bench-jmh/src/main/scala/akka/http/impl/engine/http2/H2ClientServerBenchmark.scala
+++ akka-http-bench-jmh/src/main/scala/akka/http/impl/engine/http2/H2ClientServerBenchmark.scala
@@ -7,23 +7,16 @@ package akka.http.impl.engine.http2
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.http.CommonBenchmark
-import akka.http.impl.engine.http2.FrameEvent.HeadersFrame
-import akka.http.impl.engine.http2.framing.FrameRenderer
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.client.RequestBuilding.Get
-import akka.http.scaladsl.model.HttpEntity.LastChunk
-import akka.http.scaladsl.model.headers.RawHeader
-import akka.http.scaladsl.model.{ ContentTypes, HttpEntity, HttpRequest, HttpResponse }
+import akka.http.scaladsl.model.{ HttpRequest, HttpResponse }
 import akka.http.scaladsl.settings.{ ClientConnectionSettings, ServerSettings }
 import akka.stream.ActorMaterializer
 import akka.stream.TLSProtocol.{ SslTlsInbound, SslTlsOutbound }
 import akka.stream.scaladsl.{ BidiFlow, Flow, Keep, Sink, Source }
 import akka.util.ByteString
-import com.typesafe.config.ConfigFactory
 import org.openjdk.jmh.annotations._
 
 import java.util.concurrent.{ CountDownLatch, TimeUnit }
-import scala.collection.immutable
 import scala.concurrent.duration._
 import scala.concurrent.{ Await, ExecutionContext, Future }
 
@@ -31,15 +24,7 @@ import scala.concurrent.{ Await, ExecutionContext, Future }
  * Test converting a HttpRequest to bytes at the client and back to a request at the server, and vice-versa
  * for the response. Does not include the network.
  */
-class H2ClientServerBenchmark extends CommonBenchmark {
-  // Obtained by converting the input request bytes from curl with --http2-prior-knowledge
-  def request(streamId: Int) =
-    FrameRenderer.render(HeadersFrame(streamId, endStream = true, endHeaders = true, HPackSpecExamples.C41FirstRequestWithHuffman, None))
-
-  @Param(Array("closedelimited", "chunked"))
-  var responsetype: String = _
-
-  var response: HttpResponse = _
+class H2ClientServerBenchmark extends CommonBenchmark with H2RequestResponseBenchmark {
   var httpFlow: Flow[HttpRequest, HttpResponse, Any] = _
   implicit var system: ActorSystem = _
   implicit var mat: ActorMaterializer = _
@@ -54,7 +39,7 @@ class H2ClientServerBenchmark extends CommonBenchmark {
     val latch = new CountDownLatch(numRequests)
 
     val requests =
-      Source.repeat(Get("/")).take(numRequests)
+      Source.repeat(request).take(numRequests)
         .concatMat(Source.maybe)(Keep.right)
 
     val (in, done) =
@@ -73,21 +58,8 @@ class H2ClientServerBenchmark extends CommonBenchmark {
 
   @Setup
   def setup(): Unit = {
-    response = responsetype match {
-      case "closedelimited" => HPackSpecExamples.FirstResponse
-      case "chunked" => HPackSpecExamples.FirstResponse.withEntity(
-        HttpEntity.Chunked(ContentTypes.NoContentType, Source.single(LastChunk(trailer = immutable.Seq(RawHeader("grpc-status", "9")))))
-      )
-    }
-    val config =
-      ConfigFactory.parseString(
-        s"""
-           #akka.loglevel = debug
-           akka.actor.default-dispatcher.fork-join-executor.parallelism-max = 1
-           #akka.http.server.log-unencrypted-network-bytes = 100
-           akka.http.server.http2.max-concurrent-streams = $numRequests # needs to be >= `numRequests`
-         """)
-        .withFallback(ConfigFactory.load())
+    initRequestResponse()
+
     system = ActorSystem("AkkaHttpBenchmarkSystem", config)
     mat = ActorMaterializer()
     val settings = implicitly[ServerSettings]
