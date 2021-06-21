f23a9b49093ed1f14b230de02ce54ca39fc1060b
Johannes Rudolph <johannes.rudolph@gmail.com>
2021-06-21 15:42:58 +0200

    more cosmetics

--- akka-http-core/src/main/scala/akka/http/impl/engine/ws/FrameLogger.scala
+++ akka-http-core/src/main/scala/akka/http/impl/engine/ws/FrameLogger.scala
@@ -33,8 +33,11 @@ private[ws] object FrameLogger {
   def logEvent(frameEvent: FrameEventOrError): String = {
     import Console._
 
-    def displayLogEntry(frameType: String, length: Long, data: String, lastPart: Boolean, flags: Option[String]*): String =
-      f"$GREEN$frameType%s $RED${flags.flatten.mkString(" ")} $YELLOW$length%4d bytes $RESET $data${if (!lastPart) " ..." else ""}"
+    def displayLogEntry(frameType: String, length: Long, data: String, lastPart: Boolean, flags: Option[String]*): String = {
+      val f = if (flags.nonEmpty) s" $RED${flags.flatten.mkString(" ")}" else ""
+      val l = if (length > 0) f" $YELLOW$length%d bytes" else ""
+      f"$GREEN$frameType%s$f$l$RESET $data${if (!lastPart) " ..." else ""}"
+    }
 
     def flag(value: Boolean, name: String): Option[String] = if (value) Some(name) else None
     def hex(bytes: ByteString): String = {
@@ -48,7 +51,7 @@ private[ws] object FrameLogger {
 
     frameEvent match {
       case f @ FrameStart(header, data) => displayLogEntry(header.opcode.short, header.length, hex(data), f.lastPart, flag(header.fin, "FIN"), flag(header.rsv1, "RSV1"), flag(header.rsv2, "RSV2"), flag(header.rsv3, "RSV3"))
-      case FrameData(data, lastPart)    => displayLogEntry("...", 0, hex(data), lastPart)
+      case FrameData(data, lastPart)    => displayLogEntry("DATA", 0, hex(data), lastPart)
       case FrameError(ex) =>
         f"${RED}Error: ${ex.getMessage}$RESET"
     }
