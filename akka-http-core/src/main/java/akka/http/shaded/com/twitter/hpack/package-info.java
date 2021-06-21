da2a838696c2362d95960c838dafbb06a3fec020
Akka Jenkins Autoformatter <johannes.rudolph@gmail.com>
2021-06-21 14:21:13 +0000

    Autoformatted code

--- akka-http-core/src/main/java/akka/http/shaded/com/twitter/hpack/package-info.java
+++ akka-http-core/src/main/java/akka/http/shaded/com/twitter/hpack/package-info.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 /**
  * <a href="http://tools.ietf.org/html/rfc7541">HPACK: Header Compression for HTTP/2</a>
  */
--- akka-http-core/src/main/scala/akka/http/impl/engine/server/NewTcpRegistry.scala
+++ akka-http-core/src/main/scala/akka/http/impl/engine/server/NewTcpRegistry.scala
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2021 Lightbend Inc. <https://www.lightbend.com>
+ */
+
 package akka.http.impl.engine.server
 
 import akka.actor.ExtendedActorSystem
