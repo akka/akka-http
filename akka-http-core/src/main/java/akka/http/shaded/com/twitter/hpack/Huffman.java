8824bcefa1bfe095e3fbdbac3609adc3bafd904c
Akka Jenkins Autoformatter <johannes.rudolph@gmail.com>
2021-06-22 08:59:00 +0000

    Autoformatted code

--- akka-http-core/src/main/java/akka/http/shaded/com/twitter/hpack/Huffman.java
+++ akka-http-core/src/main/java/akka/http/shaded/com/twitter/hpack/Huffman.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package akka.http.shaded.com.twitter.hpack;
 
 import static akka.http.shaded.com.twitter.hpack.HpackUtil.HUFFMAN_CODE_LENGTHS;
--- akka-http-core/src/main/java/akka/http/shaded/com/twitter/hpack/HuffmanDecoder.java
+++ akka-http-core/src/main/java/akka/http/shaded/com/twitter/hpack/HuffmanDecoder.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package akka.http.shaded.com.twitter.hpack;
 
 import java.io.ByteArrayOutputStream;
