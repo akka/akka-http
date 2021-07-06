c47eade613745a886f01ee4f0dbcbf1525981084
Johannes Rudolph <johannes.rudolph@gmail.com>
2021-07-06 12:10:22 +0200

    add StringTools.asciiStringBytes

--- akka-http-core/src/main/scala/akka/http/impl/util/StringTools.scala
+++ akka-http-core/src/main/scala/akka/http/impl/util/StringTools.scala
@@ -1,6 +1,7 @@
 package akka.http.impl.util
 
 import akka.annotation.InternalApi
+import akka.util.Unsafe
 import com.github.ghik.silencer.silent
 
 /**
@@ -13,4 +14,10 @@ private[http] object StringTools {
     // Deprecated constructor but also (unfortunately) the fastest way to convert a ASCII encoded byte array
     // into a String without extra copying.
     new String(bytes, 0)
+
+  def asciiStringBytes(string: String): Array[Byte] = {
+    val bytes = new Array[Byte](string.length)
+    Unsafe.copyUSAsciiStrToBytes(string, bytes)
+    bytes
+  }
 }
