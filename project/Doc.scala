690442d8966670249de9eb984d8a8fe12d7fdb09
Johannes Rudolph <johannes.rudolph@gmail.com>
2021-07-13 10:22:34 +0200

    build: update dependencies

--- project/Doc.scala
+++ project/Doc.scala
@@ -172,7 +172,7 @@ object BootstrapGenjavadoc extends AutoPlugin {
       javacOptions in test += "-Xdoclint:none",
       javacOptions in doc += "-Xdoclint:none",
       scalacOptions in Compile += "-P:genjavadoc:fabricateParams=true",
-      unidocGenjavadocVersion in Global := "0.16"
+      unidocGenjavadocVersion in Global := "0.17"
     )
   ).getOrElse(Seq.empty)
 }
