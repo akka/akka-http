e652652288da332bc85a49b1000e048a7a402085
Johannes Rudolph <johannes.rudolph@gmail.com>
2021-09-09 12:11:23 +0200

    compiling pre213 / since213 annotations

---
+++ akka-parsing/src/main/scala-3/akka/http/ccompat/pre213macro.scala
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2019-2021 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.http.ccompat
+
+import scala.annotation.StaticAnnotation
+
+// FIXME: need to implement or we will resurrect some ugly things (or remove 2.12 support)
+class pre213 extends StaticAnnotation
