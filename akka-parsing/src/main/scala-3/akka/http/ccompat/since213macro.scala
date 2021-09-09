fee4a47755c75c34a494d052a51077bf069efd62
Johannes Rudolph <johannes.rudolph@gmail.com>
2021-09-09 12:11:23 +0200

    parsing: Scala 3 pre213 / since213 annotations (without function)

---
+++ akka-parsing/src/main/scala-3/akka/http/ccompat/since213macro.scala
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2019-2021 Lightbend Inc. <https://www.lightbend.com>
+ */
+
+package akka.http.ccompat
+
+import scala.annotation.StaticAnnotation
+
+class since213 extends StaticAnnotation
