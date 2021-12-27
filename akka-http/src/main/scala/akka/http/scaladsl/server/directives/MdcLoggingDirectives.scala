629938feaa7b913ba914c7a6b4ac9ce69e5f2e70
Alex Klibisz <aklibisz@tesla.com>
2021-12-27 09:29:57 -0600

    Rearrange methods to get rid of .asInstanceOf

--- akka-http/src/main/scala/akka/http/scaladsl/server/directives/MdcLoggingDirectives.scala
+++ akka-http/src/main/scala/akka/http/scaladsl/server/directives/MdcLoggingDirectives.scala
@@ -15,49 +15,48 @@ import akka.http.scaladsl.server._
  */
 trait MdcLoggingDirectives {
 
-  def withMdcLogging: Directive0 =
-    extractActorSystem
-      .flatMap { sys =>
-        mapRequestContext { ctx =>
-          ctx.log match {
-            // If it's already a DiagnosticMarkerBusLoggingAdapter, this is a no-op.
-            case _: DiagnosticMarkerBusLoggingAdapter => ctx
-            // Otherwise, we need to give the context a DiagnosticLoggingAdapter.
-            case _ =>
-              val (str, cls) = LogSource.fromAnyRef(sys, sys)
-              val loggingFilter =
-                new DefaultLoggingFilter(sys.settings, sys.eventStream)
-              val dmbla = new DiagnosticMarkerBusLoggingAdapter(
-                sys.eventStream,
-                str,
-                cls,
-                loggingFilter)
-              ctx.withLog(dmbla)
-          }
-        }
+  import MdcLoggingDirectives.extractDiagnosticMarkerLog
+
+  def withMdcLogging: Directive0 = extractDiagnosticMarkerLog
+    .flatMap { dmbla: DiagnosticMarkerBusLoggingAdapter =>
+      mapRequestContext { ctx =>
+        ctx.withLog(dmbla)
       }
+    }
 
   def extractMarkerLog: Directive1[MarkerLoggingAdapter] =
-    MdcLoggingDirectives.extractDiagnosticMarkerLog
-      .map(identity[MarkerLoggingAdapter])
+    withMdcLogging.tflatMap { _ =>
+      extractDiagnosticMarkerLog
+        .map(identity[MarkerLoggingAdapter])
+    }
 
   def withMdcEntries(entries: (String, Any)*): Directive0 =
-    MdcLoggingDirectives.extractDiagnosticMarkerLog
-      .map(log => log.mdc(log.mdc ++ entries.toMap))
+    withMdcLogging.tflatMap { _ =>
+      extractDiagnosticMarkerLog
+        .map(log => log.mdc(log.mdc ++ entries.toMap))
+    }
 
   def withMdcEntry(key: String, value: Any): Directive0 =
     withMdcEntries((key, value))
 }
 
 object MdcLoggingDirectives extends MdcLoggingDirectives {
-  // This is private because the only advantage of a DiagnosticLoggingAdapter is that you can mutate the MDC entries,
-  // which is better done through the withMdcEntry and withMdcEntries directive.
   private val extractDiagnosticMarkerLog: Directive1[DiagnosticMarkerBusLoggingAdapter] =
-    withMdcLogging.tflatMap { _ =>
-      extractLog.flatMap { log =>
-        // This asInstanceOf is tested and virtually guaranteed to work, but is there any way to do it without casting?
-        val dmbla = log.asInstanceOf[DiagnosticMarkerBusLoggingAdapter]
-        provide(dmbla)
+    extractActorSystem
+      .flatMap { sys =>
+        extractRequestContext
+          .flatMap { ctx =>
+            provide {
+              ctx.log match {
+                // If it's already a DiagnosticMarkerBusLoggingAdapter, this is a no-op.
+                case dmbla: DiagnosticMarkerBusLoggingAdapter => dmbla
+                // Otherwise, we need to construct a DiagnosticLoggingAdapter.
+                case _ =>
+                  val (str, cls) = LogSource.fromAnyRef(sys, sys)
+                  val filter = new DefaultLoggingFilter(sys.settings, sys.eventStream)
+                  new DiagnosticMarkerBusLoggingAdapter(sys.eventStream, str, cls, filter)
+              }
+            }
+          }
       }
-    }
 }
