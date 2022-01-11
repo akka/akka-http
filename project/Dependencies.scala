d2e0c72bee4b0153156ecce74d62e598f27235c9
Scala Steward <me@scala-steward.org>
2022-01-11 02:05:18 +0100

    build: update scala-xml from 1.3.0 to 2.0.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -44,7 +44,7 @@ object Dependencies {
   }
 
   object Compile {
-    val scalaXml      = "org.scala-lang.modules"      %% "scala-xml"                   % "1.3.0" // Scala License
+    val scalaXml      = "org.scala-lang.modules"      %% "scala-xml"                   % "2.0.1" // Scala License
 
     // For akka-http spray-json support
     val sprayJson   = "io.spray"                     %% "spray-json"                   % "1.3.6"       // ApacheV2
