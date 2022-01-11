e0f81e67ed8c3bbe86db1c7f5485cd736e0de2ac
Scala Steward <me@scala-steward.org>
2022-01-11 02:05:07 +0100

    build: update reflections from 0.9.12 to 0.10.2

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -63,7 +63,7 @@ object Dependencies {
       val sprayJson   = Compile.sprayJson                                                                    % "test"
       val gson        = "com.google.code.gson"             % "gson"                    % "2.8.7"             % "test"
       val jacksonXml  = "com.fasterxml.jackson.dataformat" % "jackson-dataformat-xml"  % jacksonXmlVersion      % "test" // ApacheV2
-      val reflections = "org.reflections"                  % "reflections"             % "0.9.12"            % "test" // WTFPL
+      val reflections = "org.reflections"                  % "reflections"             % "0.10.2"            % "test" // WTFPL
     }
 
     object Test {
