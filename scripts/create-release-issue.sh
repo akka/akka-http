2dd1b176fbf9f83b1fe51105026feb642a31b006
Johan Andrén <johan@markatta.com>
2022-11-10 16:11:09 +0100

    build: Fix release script typo (#4188)

--- scripts/create-release-issue.sh
+++ scripts/create-release-issue.sh
@@ -5,6 +5,6 @@ if [ -z $VERSION ]
 then
   echo specify the version name to be released, eg. 10.5.0
 else
-  sed -e 's/\$VERSION\$/'$VERSION'/g' docs/release-train-issue-template.md > /tmp/release-$VERSION.tmp
+  sed -e 's/\$VERSION\$/'$VERSION'/g' docs/release-train-issue-template.md > /tmp/release-$VERSION.md
   echo Created $(gh issue create --title "Release $VERSION" --body-file /tmp/release-$VERSION.md --milestone $VERSION --web)
 fi
