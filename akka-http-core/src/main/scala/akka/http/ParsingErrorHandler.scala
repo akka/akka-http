acb25f0155b4bb29ed9142ac2246c1c745d59825
Johan Andrén <johan@markatta.com>
2022-11-21 18:04:09 +0100

    Dummy commit to trigger PR validation

--- akka-http-core/src/main/scala/akka/http/ParsingErrorHandler.scala
+++ akka-http-core/src/main/scala/akka/http/ParsingErrorHandler.scala
@@ -9,6 +9,8 @@ import akka.http.javadsl.{ model => jm }
 import akka.http.scaladsl.model.{ ErrorInfo, HttpResponse, StatusCode }
 import akka.http.scaladsl.settings.ServerSettings
 
+// FIXME trigger PR validation
+
 abstract class ParsingErrorHandler {
   def handle(status: StatusCode, error: ErrorInfo, log: LoggingAdapter, settings: ServerSettings): jm.HttpResponse
 }
