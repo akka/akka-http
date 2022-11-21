5ca2c680aaba269a5fbe81c87dd0e29a92301acd
Johan Andrén <johan@markatta.com>
2022-11-21 19:47:51 +0100

    chore: Undo dummy commit (#4201)

--- akka-http-core/src/main/scala/akka/http/ParsingErrorHandler.scala
+++ akka-http-core/src/main/scala/akka/http/ParsingErrorHandler.scala
@@ -9,8 +9,6 @@ import akka.http.javadsl.{ model => jm }
 import akka.http.scaladsl.model.{ ErrorInfo, HttpResponse, StatusCode }
 import akka.http.scaladsl.settings.ServerSettings
 
-// FIXME trigger PR validation
-
 abstract class ParsingErrorHandler {
   def handle(status: StatusCode, error: ErrorInfo, log: LoggingAdapter, settings: ServerSettings): jm.HttpResponse
 }
