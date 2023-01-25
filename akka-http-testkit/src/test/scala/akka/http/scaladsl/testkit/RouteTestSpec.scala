dcc1f0b4b6265f2ed1d03b36c3a7fafd0972acd6
Johan Andrén <johan@markatta.com>
2023-01-25 15:46:02 +0100

    Headers

--- akka-http-testkit/src/test/scala/akka/http/scaladsl/testkit/RouteTestSpec.scala
+++ akka-http-testkit/src/test/scala/akka/http/scaladsl/testkit/RouteTestSpec.scala
@@ -1,6 +1,7 @@
 /*
- * Copyright (C) 2009-2023 Lightbend Inc. <http://www.lightbend.com>
+ * Copyright (C) 2009-2022 Lightbend Inc. <https://www.lightbend.com>
  */
+
 package akka.http.scaladsl.testkit
 
 import akka.actor.ActorSystem
