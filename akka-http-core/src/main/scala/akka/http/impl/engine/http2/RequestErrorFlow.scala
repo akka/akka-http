39f13679f6981a63fefecd3b15ed02089c0093c0
Johan Andrén <johan@markatta.com>
2023-02-17 14:45:38 +0100

    Align headers

--- akka-http-core/src/main/scala/akka/http/impl/engine/http2/RequestErrorFlow.scala
+++ akka-http-core/src/main/scala/akka/http/impl/engine/http2/RequestErrorFlow.scala
@@ -1,6 +1,7 @@
 /*
- * Copyright (C) 2009-2023 Lightbend Inc. <https://www.lightbend.com>
+ * Copyright (C) 2009-2022 Lightbend Inc. <https://www.lightbend.com>
  */
+
 package akka.http.impl.engine.http2
 
 import akka.NotUsed
