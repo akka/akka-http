2ad1694eee85337bb6271816e06b659e99fe1a11
Eduardo Pinto <efgpinto@gmail.com>
2024-04-11 22:13:32 +0100

    Update akka-http-jwt/src/main/scala/akka/http/jwt/javadsl/JwtSettings.scala
    Co-authored-by: Johan Andrén <johan@markatta.com>

--- akka-http-jwt/src/main/scala/akka/http/jwt/javadsl/JwtSettings.scala
+++ akka-http-jwt/src/main/scala/akka/http/jwt/javadsl/JwtSettings.scala
@@ -7,6 +7,7 @@ package akka.http.jwt.javadsl
 import akka.annotation.{ ApiMayChange, DoNotInherit, InternalApi }
 import akka.http.jwt.internal.{ JwtSettingsImpl, JwtSupport }
 
+/** Not for user extension */
 @ApiMayChange @DoNotInherit
 abstract class JwtSettings private[akka] { self: JwtSettingsImpl =>
   /** INTERNAL API */
