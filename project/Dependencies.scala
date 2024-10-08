367e99d139f63fda793f568ad09d749556921f8a
Scala Steward <scala_steward@virtuslab.com>
2024-10-08 13:06:55 +0000

    bump: update scala3-library from 3.3.3 to 3.3.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -26,7 +26,7 @@ object Dependencies {
   val scalafixVersion = _root_.scalafix.sbt.BuildInfo.scalafixVersion // grab from plugin
 
   val scala213Version = "2.13.14"
-  val scala3Version = "3.3.3"
+  val scala3Version = "3.3.4"
   val allScalaVersions = Seq(scala213Version, scala3Version)
 
   val Versions = Seq(
