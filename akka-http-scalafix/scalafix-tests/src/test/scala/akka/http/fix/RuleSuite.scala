f2181ba58fc6d5035e12d5c450d9d2b16c6550ea
Johan Andrén <johan@markatta.com>
2025-10-27 09:21:17 +0100

    chore: Make scalafix parts compile and test on 2.13.7

--- akka-http-scalafix/scalafix-tests/src/test/scala/akka/http/fix/RuleSuite.scala
+++ akka-http-scalafix/scalafix-tests/src/test/scala/akka/http/fix/RuleSuite.scala
@@ -4,9 +4,9 @@
 
 package akka.http.fix
 
-import org.scalatest.FunSpecLike
+import org.scalatest.funspec.AnyFunSpecLike
 import scalafix.testkit.AbstractSemanticRuleSuite
 
-class RuleSuite extends AbstractSemanticRuleSuite with FunSpecLike {
+class RuleSuite extends AbstractSemanticRuleSuite with AnyFunSpecLike {
   runAllTests()
 }
