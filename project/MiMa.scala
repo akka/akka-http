2a7ea8f54d6237028c5406388964a79e6db1fc05
Johan Andrén <johan@markatta.com>
2025-11-05 13:54:27 +0100

    build: MiMa update after 10.7.3 release

--- project/MiMa.scala
+++ project/MiMa.scala
@@ -58,7 +58,8 @@ object MiMa extends AutoPlugin {
   val `10.7-versions` = Set(
     "10.7.0",
     "10.7.1",
-    "10.7.2"
+    "10.7.2",
+    "10.7.3
   )
 
   val pre3Versions = `10.2-versions` ++ `10.4-versions`
